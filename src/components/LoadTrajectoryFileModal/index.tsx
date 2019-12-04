099716ad5a3db6e9ddaa982d42f3315a38262c77
Megan Riel-Mehan <meganrm@gmail.com>
2019-12-03 16:05:19 -0800

    added file load modal

---
+++ src/components/LoadTrajectoryFileModal/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { ActionCreator } from "redux";
+import { Modal, Button } from "antd";
+
+import { TRAJECTORY_FILES } from "../../constants";
+import { ToggleAction } from "../../state/selection/types";
+
+interface LoadTrajectoryFileModalProps {
+    visible: boolean;
+    selectFile: (newFile: string) => void;
+    closeModal: ActionCreator<ToggleAction>;
+}
+
+class LoadTrajectoryFileModal extends React.Component<
+    LoadTrajectoryFileModalProps
+> {
+    render() {
+        const { visible, closeModal, selectFile } = this.props;
+
+        return (
+            <div>
+                <Modal
+                    title="Basic Modal"
+                    visible={visible}
+                    onOk={closeModal}
+                    onCancel={closeModal}
+                >
+                    {TRAJECTORY_FILES.map((fileName) => (
+                        <Button
+                            key={fileName}
+                            onClick={() => selectFile(`${fileName}.h5`)}
+                        >
+                            {fileName}
+                        </Button>
+                    ))}
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default LoadTrajectoryFileModal;
