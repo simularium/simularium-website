1dd2b3fb669760f52810253fb90be5adce9b5471
meganrm <meganrm@gmail.com>
2020-08-06 12:55:44 -0700

    typing as a react component so obj has constructor

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,9 +1,14 @@
 import { forOwn, isFunction } from "lodash";
+import React from "react";
 
 type AnyFunction = () => any;
 
-export function bindAll<T>(obj: T, methods: AnyFunction[]) {
+export function bindAll<T extends React.Component>(
+    obj: T,
+    methods: AnyFunction[]
+) {
     const setOfMethods = new Set(methods);
+
     forOwn(obj.constructor.prototype, (value, key) => {
         if (setOfMethods.has(value) && isFunction(value)) {
             Object.assign(obj, { [key]: value.bind(obj) });
