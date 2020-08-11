ca569e77c542f8ed8472b4d8d98e2d36f1f6eac6
meganrm <meganrm@gmail.com>
2020-08-11 14:32:03 -0700

    added router switch

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,25 @@ import "core-js/es6/set";
 import * as React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { APP_ID } from "./constants";
-import App from "./containers/App";
+import Simularium from "./containers/Simularium";
+import LandingPage from "./containers/LandingPage";
 import { createReduxStore } from "./state";
 
 render(
     <Provider store={createReduxStore()}>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <LandingPage />
+                </Route>
+                <Route path="/viewer">
+                    <Simularium />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById(APP_ID)
 );
