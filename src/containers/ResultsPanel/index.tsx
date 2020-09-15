74ce7b74499a77ae966cc4d6d8d6ec22360fd8d4
meganrm <meganrm@gmail.com>
2020-09-14 18:57:50 -0700

    removed default plot request

--- src/containers/ResultsPanel/index.tsx
+++ src/containers/ResultsPanel/index.tsx
@@ -2,28 +2,21 @@ import * as React from "react";
 import { ActionCreator } from "redux";
 import CollaspableMenu from "../../components/CollapseableMenu";
 
-import Graphing from "../../components/Graphing";
+import Graphing from "../../components/Plots";
 import { getCurrentTime } from "../../state/selection/selectors";
 import { State } from "../../state/types";
 import { connect } from "react-redux";
 import { changeTime } from "../../state/selection/actions";
 import { ChangeTimeAction } from "../../state/selection/types";
-import { requestMetadata } from "../../state/metadata/actions";
-import { RequestAction } from "../../state/metadata/types";
 import { getPlotData } from "../../state/metadata/selectors";
 
 interface ResultsPanelProps {
     plotData: any;
-    requestMetadata: ActionCreator<RequestAction>;
     time: number;
     changeTime: ActionCreator<ChangeTimeAction>;
 }
 
 class ResultsPanel extends React.Component<ResultsPanelProps, {}> {
-    public componentDidMount() {
-        const { requestMetadata } = this.props;
-        requestMetadata();
-    }
     public render(): JSX.Element {
         const { changeTime, time, plotData } = this.props;
         return (
@@ -54,7 +47,6 @@ function mapStateToProps(state: State) {
 
 const dispatchToPropsMap = {
     changeTime,
-    requestMetadata,
 };
 
 export default connect(
