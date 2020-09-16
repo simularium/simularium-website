4f8eacff1f1ef978edaead7816f201bd551fad5e
meganrm <meganrm@gmail.com>
2020-09-15 17:13:45 -0700

    leaving the nav bar for now

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,25 @@ import "core-js/es6/set";
 import * as React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { Layout } from "antd";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { APP_ID } from "./constants";
 
 import { createReduxStore } from "./state";
 import routes from "./routes";
-import { Layout } from "antd";
+import NavBar from "./components/NavBar";
 
-const { Footer } = Layout;
+const { Header, Footer } = Layout;
 render(
     <Provider store={createReduxStore()}>
         <Layout>
             <BrowserRouter
                 basename={process.env.GH_BUILD ? "/simularium-website/" : ""}
             >
+                <Header>
+                    <NavBar />
+                </Header>
                 <Switch>
                     {routes.map((route) => (
                         <Route
