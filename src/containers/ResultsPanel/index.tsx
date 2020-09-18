816d1f83a22565e57d65a081d870b7b9b367a8b3
meganrm <meganrm@gmail.com>
2020-09-18 15:15:02 -0700

    update import name

--- src/containers/ResultsPanel/index.tsx
+++ src/containers/ResultsPanel/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ActionCreator } from "redux";
-import CollaspableMenu from "../../components/CollapseableMenu";
+import CollaspableMenu from "../../components/NestedMenus";
 
 import Graphing from "../../components/Plots";
 import { getCurrentTime } from "../../state/selection/selectors";
