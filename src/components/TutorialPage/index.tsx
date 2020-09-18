41173bca417ab66e491676f892f107badaa2c82e
Na Hyung Choi <schoinh@gmail.com>
2020-09-17 17:47:53 -0700

    Add content to tutorial page

--- src/components/TutorialPage/index.tsx
+++ src/components/TutorialPage/index.tsx
@@ -1,99 +1,157 @@
 import * as React from "react";
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+
+import dragDropImage from "../../assets/drag-drop.gif";
+import { UploadFile } from "../Icons";
 
 const { Content } = Layout;
+const { Text } = Typography;
 
 const styles = require("./style.css");
 
-const LandingPage = (): JSX.Element => (
-    <Layout className={styles.container}>
-        <Content className={styles.content}>
-            <ul>
-                <li>Drag and drop example data</li>
-                <ol>
-                    <li>Download example trajectories from http://#</li>
+const TutorialPage: React.FunctionComponent<{}> = () => {
+    return (
+        <Layout className={styles.container}>
+            <Content className={styles.content}>
+                <br />
+                <h1>Getting Started with Simularium</h1>
+                <br />
+                <ul>
                     <li>
-                        In a web browser (Chrome or Firefox), navigate to
-                        http://simularium.allencell.org/viewer
-                    </li>
-                    <li>
-                        Drag one of the downloaded files from your file browser
-                        onto the the data input icon [image of icon]
-                    </li>
-                </ol>
-                <li>Convert your data into Simularium format</li>
-                <ol>
-                    <li>
-                        Install Anaconda if you have not already:
-                        https://docs.conda.io/en/latest/miniconda.html
-                    </li>
-                    <li>
-                        In a terminal window, create and activate a new Conda
-                        environment:
+                        Drag and drop example data
                         <br />
-                        conda create -n simularium python=3
-                        <br />
-                        conda activate simularium
+                        <img
+                            className={styles.dragDropImage}
+                            src={dragDropImage}
+                        />
                     </li>
-                    <li>
-                        Install the simulariumio Python package:
-                        <br />
-                        pip install simulariumio
-                    </li>
-                    <li>
-                        In your Python script, import simulariumio:
-                        <br />
-                        from simulariumio import Converter
-                    </li>
-                    <li>
-                        If you used one of our supported simulators to generate
-                        your data, you can use the conversion function:
-                        <br />
-                        Converter(example_data,
-                        &apos;cytosim&apos;).write_JSON(output_path)
-                        <ul>
-                            <li>We support the following simulators:</li>
+                    <ol>
+                        <li>
+                            Download example trajectories from{" "}
+                            <a href="https://#">[URL]</a>.
+                        </li>
+                        <li>
+                            In a web browser (Chrome or Firefox), navigate to{" "}
+                            <a href="http://simularium.allencell.org/viewer">
+                                http://simularium.allencell.org/viewer
+                            </a>
+                            .
+                        </li>
+                        <li>
+                            Drag one of the downloaded files from your file
+                            browser onto the the data input icon {UploadFile}.
+                        </li>
+                    </ol>
+                </ul>
+                <ul>
+                    <li>Convert your data into Simularium format</li>
+                    <ol>
+                        <li>
+                            Install Anaconda if you have not already:{" "}
+                            <a href="https://docs.conda.io/en/latest/miniconda.html">
+                                https://docs.conda.io/en/latest/miniconda.html
+                            </a>
+                        </li>
+                        <li>
+                            In a terminal window, create and activate a new
+                            conda environment:
+                            <p>
+                                <Text code>
+                                    conda create -n simularium python=3
+                                </Text>
+                                <br />
+                                <Text code>conda activate simularium</Text>
+                            </p>
+                        </li>
+                        <li>
+                            Install the simulariumio Python package:
+                            <p>
+                                <Text code>pip install simulariumio</Text>
+                            </p>
+                        </li>
+                        <li>
+                            In your Python script, import simulariumio:
+                            <p>
+                                <Text code>
+                                    from simulariumio import Converter
+                                </Text>
+                            </p>
+                        </li>
+                        <li>
+                            If you used one of our supported simulators to
+                            generate your data, you can use the conversion
+                            function:
+                            <p>
+                                <Text code>
+                                    Converter(example_data,
+                                    &apos;cytosim&apos;).write_JSON(output_path)
+                                </Text>
+                            </p>
                             <ul>
-                                <li>ReaDDy (https://readdy.github.io/)</li>
-                                <li>PhysiCell (http://physicell.org/)</li>
-                                <li>
-                                    CytoSim
-                                    (https://gitlab.com/f.nedelec/cytosim)
-                                </li>
+                                <li>We support the following simulators:</li>
+                                <ul>
+                                    <li>
+                                        ReaDDy (
+                                        <a href="https://readdy.github.io/">
+                                            https://readdy.github.io/
+                                        </a>
+                                        )
+                                    </li>
+                                    <li>
+                                        PhysiCell (
+                                        <a href="http://physicell.org/">
+                                            http://physicell.org/
+                                        </a>
+                                        )
+                                    </li>
+                                    <li>
+                                        CytoSim (
+                                        <a href="https://gitlab.com/f.nedelec/cytosim">
+                                            https://gitlab.com/f.nedelec/cytosim
+                                        </a>
+                                        )
+                                    </li>
+                                </ul>
                             </ul>
-                        </ul>
-                    </li>
-                    <li>
-                        If you used another simulator to generate your data, you
-                        can use the generic conversion function:
-                        <br />
-                        Converter(example_data).write_JSON(output_path)
-                    </li>
-                    <li>
-                        These{" "}
-                        <a href="https://github.com/allen-cell-animated/simulariumio/blob/feature/convert-custom-data/simulariumio/bin/Tutorial.ipynb">
-                            Jupyter notebooks
-                        </a>{" "}
-                        provide examples of saving data from different sources
-                        in Simularium’s input format.
-                    </li>
-                    <li>
-                        Run your python script and notice a file generated at
-                        the output path you provided.
-                    </li>
-                    <li>
-                        In a web browser (Chrome or Firefox), navigate to
-                        http://simularium.allencell.org/viewer
-                    </li>
-                    <li>
-                        Drag the resulting file from your file browser onto the
-                        the data input icon [image of icon] or use the file
-                        upload dialogue to choose your file.
-                    </li>
-                </ol>
-            </ul>
-        </Content>
-    </Layout>
-);
+                        </li>
+                        <li>
+                            If you used another simulator to generate your data,
+                            you can use the generic conversion function:
+                            <p>
+                                <Text code>
+                                    Converter(example_data).write_JSON(output_path)
+                                </Text>
+                            </p>
+                        </li>
+                        <li>
+                            These{" "}
+                            <a href="https://github.com/allen-cell-animated/simulariumio/blob/feature/convert-custom-data/simulariumio/bin/Tutorial.ipynb">
+                                Jupyter notebooks
+                            </a>{" "}
+                            provide examples of saving data from different
+                            sources in Simularium’s input format.
+                        </li>
+                        <li>
+                            Run your Python script and notice a file generated
+                            at the output path you provided.
+                        </li>
+                        <li>
+                            In a web browser (Chrome or Firefox), navigate to{" "}
+                            <a href="http://simularium.allencell.org/viewer">
+                                http://simularium.allencell.org/viewer
+                            </a>
+                            .
+                        </li>
+                        <li>
+                            Drag the resulting file from your file browser onto
+                            the the data input icon {UploadFile} or use the file
+                            upload dialogue to choose your file.
+                        </li>
+                    </ol>
+                </ul>
+            </Content>
+        </Layout>
+    );
+};
 
-export default LandingPage;
+export default TutorialPage;
