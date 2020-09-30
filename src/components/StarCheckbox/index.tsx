71d1060dfc6d4d125b91243207a2e2a0a02bb67d
meganrm <meganrm@gmail.com>
2020-09-30 13:40:18 -0700

    making change handler inline

--- src/components/StarCheckbox/index.tsx
+++ src/components/StarCheckbox/index.tsx
@@ -17,22 +17,16 @@ const StarCheckbox: React.FunctionComponent<CheckboxProps> = ({
         [styles.checked]: checked,
         [styles.indeterminate]: indeterminate,
     });
-    const handleCheckboxChange = (event: any) => {
-        /* typescript compatibility issue between ChangeEvent<HTMLInput>
-         * and CheckboxChangeEvent.
-         * This solves the issue, but there is probably a better way
-         */
-        if (onChange) {
-            onChange(event as CheckboxChangeEvent);
-        }
-    };
+
     return (
         <label className={wrapperClassnames}>
             <span className={styles.container}>
                 <input
                     checked={checked}
                     type="checkbox"
-                    onChange={handleCheckboxChange}
+                    onChange={(e: any) =>
+                        onChange ? onChange(e as CheckboxChangeEvent) : null
+                    }
                     value={value}
                 />
                 <span className={checkboxClassNames} />
