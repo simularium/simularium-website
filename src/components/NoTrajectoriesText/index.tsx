57456967d30bb490222068b8d1b5733ed90f6422
Na Hyung Choi <schoinh@gmail.com>
2020-10-08 12:32:47 -0700

    Update text and apply styling

--- src/components/NoTrajectoriesText/index.tsx
+++ src/components/NoTrajectoriesText/index.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
+const styles = require("./style.css");
+
 const NoTrajectoriesText: React.FunctionComponent<{}> = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h3>No trajectories loaded</h3>
             <p>
-                To try out the Simularium Viewer, either download our example
-                data or convert your own data, then drag and drop it onto the
-                viewer.
+                To try out the Simularium Viewer, either{" "}
+                <a href="https://aics-agentviz-data.s3.us-east-2.amazonaws.com/trajectory/akamatsu_0020.simularium">
+                    download
+                </a>{" "}
+                our example data or{" "}
+                <a href="/tutorial#convert-your-data">convert</a> your own data,
+                then drag and drop it onto this window.
             </p>
             <p>
-                For details on how to prepare your data, visit{" "}
-                <Link to="/tutorial">Getting Started</Link>.
+                <Link to="/tutorial">Get started here.</Link>
             </p>
         </div>
     );
