40072e4cff5ebd37c8c4f9a466b2fb04d8fceabc
Na Hyung Choi <schoinh@gmail.com>
2020-10-12 15:08:47 -0700

    Update example trajectory items in load model dropdown

--- src/components/LoadFileMenu/index.tsx
+++ src/components/LoadFileMenu/index.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { ActionCreator } from "redux";
 import { Menu, Dropdown, Button } from "antd";
 
-import { TRAJECTORY_FILES } from "../../constants";
+import TRAJECTORIES from "../../constants/networked-trajectories";
+import { URL_PARAM_KEY_FILE_NAME } from "../../constants";
 import { RequestFileAction } from "../../state/metadata/types";
 
 import LocalFileUpload from "../LocalFileUpload";
@@ -15,25 +16,25 @@ interface NetworkFileMenuProps {
     loadLocalFile: ActionCreator<RequestFileAction>;
 }
 
-const LoadFileMenu = ({ selectFile, loadLocalFile }: NetworkFileMenuProps) => {
+const LoadFileMenu = ({ loadLocalFile }: NetworkFileMenuProps) => {
     const menu = (
         <Menu theme="dark" className={styles.menu}>
             <Menu.Item>
                 <LocalFileUpload loadLocalFile={loadLocalFile} />
             </Menu.Item>
             <Menu.SubMenu title="Load existing model">
-                {TRAJECTORY_FILES.map((fileName) => (
-                    <Menu.Item
-                        key={fileName}
-                        onClick={() => {
-                            selectFile({
-                                name: `${fileName}`,
-                                data: null,
-                                dateModified: null,
-                            });
-                        }}
-                    >
-                        <Link to="/viewer">{fileName}</Link>
+                {TRAJECTORIES.map((trajectory) => (
+                    <Menu.Item key={trajectory.id}>
+                        <Link
+                            to={{
+                                pathname: "/viewer",
+                                search: `?${URL_PARAM_KEY_FILE_NAME}=${
+                                    trajectory.id
+                                }`,
+                            }}
+                        >
+                            {trajectory.title}
+                        </Link>
                     </Menu.Item>
                 ))}
             </Menu.SubMenu>
