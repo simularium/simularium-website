3b3e65d524397ef15dce93bee23e53f747220782
meganrm <meganrm@gmail.com>
2020-10-13 12:48:12 -0700

    updated colors

--- src/containers/ViewerPanel/constants.ts
+++ src/containers/ViewerPanel/constants.ts
@@ -1,11 +1,10 @@
 export const AGENT_COLORS = [
-    "#9f516c",
     "#81dbe6",
     "#3452d8",
     "#9267cb",
     "#68a500",
-    "#d94e6f",
     "#d49a01",
+    "#9f516c",
     "#bf5736",
     "#ffc55b",
     "#ce8ec9",
@@ -13,6 +12,7 @@ export const AGENT_COLORS = [
     "#abb652",
     "#0ba345",
     "#d14040",
-    "#d98d73",
     "#418463",
+    "#7f7f7f",
+    "#333333",
 ];
