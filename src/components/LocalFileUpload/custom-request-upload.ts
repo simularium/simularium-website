9d0db54719a278568f0be1c90fbb4dca0f9c4a36
meganrm <meganrm@gmail.com>
2020-12-10 22:12:54 -0800

    set viewer loading before handeling file

--- src/components/LocalFileUpload/custom-request-upload.ts
+++ src/components/LocalFileUpload/custom-request-upload.ts
@@ -1,10 +1,13 @@
 import { RcCustomRequestOptions } from "antd/lib/upload/interface";
 import { LocalSimFile } from "../../state/metadata/types";
-
+import { setViewerStatus } from "../../state/metadata/actions";
+import { VIEWER_LOADING } from "../../state/metadata/constants";
+import { store } from "../..";
 export default (
     { file, onSuccess, onError }: RcCustomRequestOptions,
     loadFunction: (simulariumFile: LocalSimFile) => void
 ) => {
+    store.dispatch(setViewerStatus({ status: VIEWER_LOADING }));
     file.text()
         .then((text) => JSON.parse(text))
         .then((data) => {
