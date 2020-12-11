8c9a76aa50c410a4b9b3173b7feb297aed2ff03f
meganrm <meganrm@gmail.com>
2020-12-11 11:07:07 -0800

    removed the need for the await call

--- src/components/LocalFileUpload/custom-request-upload.ts
+++ src/components/LocalFileUpload/custom-request-upload.ts
@@ -1,13 +1,17 @@
 import { RcCustomRequestOptions } from "antd/lib/upload/interface";
 import { LocalSimFile } from "../../state/metadata/types";
-import { setViewerStatus } from "../../state/metadata/actions";
-import { VIEWER_LOADING } from "../../state/metadata/constants";
+import { CLEAR_SIMULARIUM_FILE } from "../../state/metadata/constants";
 import { store } from "../..";
 export default (
     { file, onSuccess, onError }: RcCustomRequestOptions,
     loadFunction: (simulariumFile: LocalSimFile) => void
 ) => {
-    store.dispatch(setViewerStatus({ status: VIEWER_LOADING }));
+    // want the loading indicator to show and clear out
+    // as soon as user hits "Open" button
+    store.dispatch({
+        payload: { newFile: true },
+        type: CLEAR_SIMULARIUM_FILE,
+    });
     file.text()
         .then((text) => JSON.parse(text))
         .then((data) => {
