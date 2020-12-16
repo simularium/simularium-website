6f2b5867fc37c74b8621340f0cac1d8a0e57497f
meganrm <meganrm@gmail.com>
2020-12-15 17:11:58 -0800

    location hook to stop play when leaving viewer

--- src/index.tsx
+++ src/index.tsx
@@ -4,24 +4,53 @@ import "core-js/es6/set";
 
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { Layout } from "antd";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, useLocation } from "react-router-dom";
 
 import { APP_ID } from "./constants";
 
 import { createReduxStore } from "./state";
-import routes from "./routes";
+import routes, { VIEWER_PATHNAME } from "./routes";
 import ScrollToTop from "./components/ScrollToTop";
 import AppHeader from "./containers/AppHeader";
 
 const { Header } = Layout;
 
 import "./style.css";
+import { setIsPlaying } from "./state/selection/actions";
 
 export const store = createReduxStore();
 
-const App = () => {
+function useLocationChangeToStopPlay() {
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        // if we're navigating away from the viewer, stop playing
+        if (location.pathname !== VIEWER_PATHNAME) {
+            dispatch(setIsPlaying(false));
+        }
+    }, [location]);
+}
+
+const RouterSwitch = () => {
+    useLocationChangeToStopPlay();
+    return (
+        <Switch>
+            {routes.map((route) => (
+                <Route
+                    key={route.path}
+                    exact={route.path === "/"}
+                    path={route.path}
+                    component={route.component}
+                />
+            ))}
+        </Switch>
+    );
+};
+
+const App: React.FunctionComponent<{}> = () => {
     /**
      * Note for implementing redirect rules for SPA routing on AWS.
      * Currently we are just serving index.html as our 400 response. If we want to have an 404.html
@@ -42,22 +71,13 @@ const App = () => {
                     <Header>
                         <AppHeader />
                     </Header>
-                    <Switch>
-                        {routes.map((route) => (
-                            <Route
-                                key={route.path}
-                                exact={route.path === "/"}
-                                path={route.path}
-                                component={route.component}
-                            />
-                        ))}
-                    </Switch>
+                    <RouterSwitch />
                 </BrowserRouter>
             </Layout>
         </Provider>
     );
 };
 
-const renderApp = () => render(App(), document.getElementById(APP_ID));
+const renderApp = () => render(<App />, document.getElementById(APP_ID));
 
 renderApp();
--- src/state/selection/logics.ts
+++ src/state/selection/logics.ts
@@ -3,12 +3,20 @@ import { createLogic } from "redux-logic";
 import { getSimulariumController } from "../metadata/selectors";
 import { ReduxLogicDeps } from "../types";
 import { SET_IS_PLAYING } from "./constants";
+import { getIsPlaying } from "./selectors";
 
 const setPlayingLogic = createLogic({
     process(deps: ReduxLogicDeps) {
         const { getState, action } = deps;
         const controller: SimulariumController = getSimulariumController(
             getState()
+        );
+        const currentlyIsPlaying = getIsPlaying(getState());
+        console.log(
+            "currently is playing",
+            currentlyIsPlaying,
+            "set play",
+            action.payload
         );
         if (action.payload && controller) {
             controller.resume();
