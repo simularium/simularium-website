ca22051e339bcbc898574106b68bff1b445872c7
Na Hyung Choi <schoinh@gmail.com>
2020-12-23 14:13:07 -0800

    Put glossary data in visual-glossary.ts

---
+++ src/constants/visual-glossary.ts
@@ -0,0 +1,61 @@
+import { VisualGlossaryItem } from "./interfaces";
+
+const visualGlossary: VisualGlossaryItem[] = [
+    {
+        label: "LOAD MODEL MENU",
+        description:
+            "Use this menu to choose an example model to view, or to select a file from your computer. You can also load a file from your computer by dragging it into the viewport.",
+    },
+    {
+        label: "CLICK TO SELECT & FOLLOW ANY OBJECT",
+        description:
+            "Left mouse clicking on any object in the 3D viewport will trigger the camera to point at it and to move to a helpful viewing distance. The camera will continue to Follow that object (outlined in bright green) until another another is selected, or all objects are deselected by clicking on an empty space in the viewport&apos;s background. It&apos;s easiest to pause the playback before attempting to CLICK on an object to follow.",
+    },
+    {
+        label: "PLAYBACK CONTROLS, from left to right:",
+        description: "",
+        bulletItems: [
+            "STEP BACK: Step to the previous frame.",
+            "PLAY/PAUSE: Play or pause playback.",
+            "STEP FORWARD: Step to the next frame.",
+        ],
+    },
+    {
+        label: "TIMELINE",
+        description:
+            "Drag the PLAYHEAD dot to a specific point in the trajectory file. Some buffering may occur, so please be patient.",
+    },
+    {
+        label: "TIME READOUT",
+        description: "Displays the current and total simulated time.",
+    },
+    {
+        label: "SCALE BAR",
+        description:
+            "Displays the distance between tickmarks on the bounding box.",
+    },
+    {
+        label: "CAMERA CONTROLS, from top to bottom:",
+        description: "",
+        bulletItems: [
+            "ZOOM IN: Move the camera in towards the camera’s current target (either the center of the simulated volume by default, or a followed object).",
+            "ZOOM OUT: Move the camera away from the camera’s target.",
+            "RESET CAMERA: Reset the 3D camera to the default (original) position and orientation.",
+        ],
+    },
+    {
+        label: "SHOW / HIDE AGENTS",
+        description: "Show or hide agents of the given type.",
+    },
+    {
+        label: "HIGHLIGHT AGENTS",
+        description:
+            "Toggle highlighting on agents of the given type with a white outline in the viewport.",
+    },
+    {
+        label: "MENU TOGGLES",
+        description: "Furl the side menus open or closed.",
+    },
+];
+
+export default visualGlossary;
