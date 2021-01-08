b0b00fa75be524b5f436f9cd1643e4810f2baea7
Na Hyung Choi <schoinh@gmail.com>
2021-01-07 17:01:19 -0800

    Feature/add tooltips to left panel (#105)

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,22 +1,61 @@
 import * as React from "react";
-import { Checkbox as AntdCheckbox } from "antd";
+import { Checkbox as AntdCheckbox, Tooltip } from "antd";
 import { CheckboxProps } from "antd/lib/checkbox";
 
 import StarCheckbox from "../StarCheckbox";
-import { CHECKBOX_TYPE_STAR } from "../../constants";
+import {
+    CHECKBOX_TYPE_STAR,
+    LEFT_PANEL_TOOLTIP_DELAY,
+    LEFT_PANEL_TOOLTIP_COLOR,
+} from "../../constants";
 
 interface CheckboxTypeProps extends CheckboxProps {
     checkboxType?: CHECKBOX_TYPE_STAR;
+    checkboxLevel?: keyof TooltipOffsets;
 }
+
+interface TooltipOffsets {
+    default: number[];
+    top: number[];
+    shared: number[];
+}
+
+const tooltipOffsets: TooltipOffsets = {
+    default: [-11, -1],
+    top: [-22, -3],
+    shared: [-8, 8],
+};
 
 const Checkbox: React.FunctionComponent<CheckboxTypeProps> = (
     props: CheckboxTypeProps
 ) => {
-    const childProps = { ...props, checkboxType: null }; // removing prop that is only needed at this level
+    // removing props that are only needed at this level
+    const childProps = { ...props, checkboxType: null, checkboxLevel: null };
+    const checkboxLevel = props.checkboxLevel ? props.checkboxLevel : "default";
+
     if (props.checkboxType === CHECKBOX_TYPE_STAR) {
+        /* 
+        Wrapping the StarCheckbox in a Tooltip component as done below for AntdCheckbox
+        doesn't work (tooltips don't appear). May be due to this requirement not being met:
+        https://ant.design/components/tooltip/#Note. So we wrap the input element
+        inside the StarCheckbox component with a Tooltip component instead. Good thing is,
+        wrapping the input element with Tooltip aligns the tooltip perfectly above the star,
+        so we don't have to manually align it as we have done for the AntdCheckbox tooltip below.
+        */
         return <StarCheckbox {...childProps} />;
     }
-    return <AntdCheckbox {...childProps} />;
+    return (
+        <Tooltip
+            title={props.checked ? "Hide" : "Show"}
+            placement="top"
+            mouseEnterDelay={LEFT_PANEL_TOOLTIP_DELAY}
+            // Position tooltip with alignConfig object: https://github.com/yiminghe/dom-align#usage
+            align={{ offset: tooltipOffsets[checkboxLevel] }}
+            color={LEFT_PANEL_TOOLTIP_COLOR}
+        >
+            <AntdCheckbox {...childProps} />
+        </Tooltip>
+    );
 };
 
 export default Checkbox;
