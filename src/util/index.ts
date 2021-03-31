ba16bd53ac5b6a66e635242a659a4e799b800645
meganrm <meganrm@gmail.com>
2021-03-31 10:06:11 -0700

    add comment about regex

--- src/util/index.ts
+++ src/util/index.ts
@@ -77,6 +77,11 @@ export const urlCheck = (urlToCheck: any): string => {
     if (typeof urlToCheck !== "string") {
         return "";
     }
+    /**
+     * RegEx: https://regexr.com/5pkui, forked from https://regexr.com/39p0t
+     * I had to modify the original to allow s3 buckets which have multiple `.letters-letters.` in them
+     * and I made the http(s) required
+     */
     const regEx = /(https?:\/\/)([\w\-]){0,200}(\.[a-zA-Z][^\-])([\/\w]*)*\/?\??([^\n\r]*)??([^\n\r]*)/g;
     if (!!urlToCheck.match(regEx)) {
         return urlToCheck;
