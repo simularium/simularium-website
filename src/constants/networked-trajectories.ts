0503927c2dd05a44b9d214b4212bdc6b8dd41aeb
Na Hyung Choi <schoinh@gmail.com>
2021-06-21 11:33:00 -0700

    Feature/add nanoparticle and Smoldyn models to landing page (#138)
    * Update networked trajectories
    
    * Display code info
    
    * Pin legalese to bottom of card
    
    * Fix whitespace on blank card
    
    * Add new thumbnails
    
    * Change p to div to remove DOM nesting warning

--- src/constants/networked-trajectories.ts
+++ src/constants/networked-trajectories.ts
@@ -5,6 +5,12 @@ import endocytosisImage from "../assets/card-images/endocytosis.png";
 import endocytosisGif from "../assets/card-images/endocytosis.gif";
 import covidImage from "../assets/card-images/covid.png";
 import covidGif from "../assets/card-images/covid.gif";
+import nanoparticleImage from "../assets/card-images/nanoparticle_wrapping_thumb.png";
+import nanoparticleGif from "../assets/card-images/nanoparticle_wrapping_thumb.gif";
+import smoldynMin1Image from "../assets/card-images/smoldyn_min1_thumb.png";
+import smoldynMin1Gif from "../assets/card-images/smoldyn_min1_thumb.gif";
+import smoldynSpineImage from "../assets/card-images/smoldyn_spine_thumb.png";
+import smoldynSpineGif from "../assets/card-images/smoldyn_spine_thumb.gif";
 // import covid19Image from "../assets/card-images/card-image-4.png";
 
 import { TrajectoryDisplayData } from "./interfaces";
@@ -25,8 +31,10 @@ const TRAJECTORIES: TrajectoryDisplayData[] = [
         },
         description:
             "A CytoSim model of a branched actin network internalizing an endocytic pit against membrane tension.",
+        code:
+            "Software used to generate this data is available <a href='https://github.com/DrubinBarnes/Akamatsu_CME_manuscript'>here</a>.",
         legalese:
-            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/DrubinBarnes/Akamatsu_CME_manuscript'>here</a>.",
+            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/DrubinBarnes/Akamatsu_CME_manuscript/blob/master/LICENSE'>here</a>.",
         imageFile: endocytosisImage,
         gifFile: endocytosisGif,
     },
@@ -46,10 +54,74 @@ const TRAJECTORIES: TrajectoryDisplayData[] = [
         },
         description:
             "A PhysiCell model of SARS-CoV-2 dynamics in human lung epithelium.",
+        code:
+            "Software used to generate this data is available <a href='https://github.com/pc4covid19/pc4covid19'>here</a>.",
         legalese:
-            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/pc4covid19/pc4covid19'>here</a>.",
+            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/pc4covid19/pc4covid19/blob/master/LICENSE'>here</a>.",
         imageFile: covidImage,
         gifFile: covidGif,
+    },
+    {
+        modelName: "Membrane Wrapping a Nanoparticle",
+        id: "nanoparticle-wrapping.simularium",
+        title: "Membrane Wrapping a Nanoparticle",
+        totalSimulatedTime: "5.48ms",
+        authors: "Mohsen Sadeghi et al.",
+        publication: {
+            title:
+                "Particle-based membrane model for mesoscopic simulation of cellular dynamics",
+            journal: "J Chem Phys",
+            year: 2018,
+            url: "https://aip.scitation.org/doi/10.1063/1.5009107",
+        },
+        description:
+            "A model of a coarse-grained particle-based membrane wrapping a nanoparticle.",
+        imageFile: nanoparticleImage,
+        gifFile: nanoparticleGif,
+    },
+    {
+        modelName: "Spatiotemporal oscillations in the E. coli Min system",
+        id: "smoldyn_min1.simularium",
+        title: "Spatiotemporal oscillations in the E. coli Min system",
+        totalSimulatedTime: "100s",
+        authors: "Steve Andrews et al.",
+        publication: {
+            title: "Detailed Simulations of Cell Biology with Smoldyn 2.1",
+            journal: "PLoS Computational Biology",
+            year: 2010,
+            url:
+                "https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1000705",
+        },
+        description:
+            "A Smoldyn example model of the E. coli Min system, which is used to find the cell center during cell division.",
+        code:
+            "Software used to generate this data is available <a href='https://github.com/ssandrews/Smoldyn'>here</a>. The input data file is <a href='https://github.com/ssandrews/Smoldyn/blob/master/examples/S99_more/Min/Min1.txt'>here</a>.",
+        legalese:
+            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/ssandrews/Smoldyn/blob/master/LICENSE'>here</a>.",
+        imageFile: smoldynMin1Image,
+        gifFile: smoldynMin1Gif,
+    },
+    {
+        modelName: "Sequestration of CaMKII in dendritic spines",
+        id: "smoldyn_spine.simularium",
+        title: "Sequestration of CaMKII in dendritic spines",
+        totalSimulatedTime: "1.5s",
+        authors: "Shahid Khan et al.",
+        publication: {
+            title: "Sequestration of CaMKII in dendritic spines in silico",
+            journal: "J Comp Neuro",
+            year: 2011,
+            url:
+                "https://link.springer.com/article/10.1007%2Fs10827-011-0323-2",
+        },
+        description:
+            "A Smoldyn model of a dendritic spine with CaMKII and molecules of the postsynaptic density at the spine tip.",
+        code:
+            "Software used to generate this data is available <a href='https://github.com/ssandrews/Smoldyn'>here</a>. The input data file is <a href='http://www.smoldyn.org/archive/Andrews_Arkin_2010/spine.txt'>here</a>.",
+        legalese:
+            "Please note that the use of this model is subject to third party licensing requirements, which are currently available <a href='https://github.com/ssandrews/Smoldyn/blob/master/LICENSE'>here</a>.",
+        imageFile: smoldynSpineImage,
+        gifFile: smoldynSpineGif,
     },
     /* Not ready yet
     {
