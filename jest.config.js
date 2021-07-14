6aa0fa990bae060ceaf137c9b479d08414796953
Na Hyung Choi <schoinh@gmail.com>
2021-07-13 18:28:49 -0700

    Rename assetsTransformer as fileTransformer

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
         "^.+\\.(js|jsx)$": "babel-jest",
     },
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js",
-        "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js",
+        "\\.(css|less)$": "<rootDir>/fileTransformer.js"
     }
 };
