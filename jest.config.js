12385520b425314b8d9c5e976225fd605ee29aef
Na Hyung Choi <schoinh@gmail.com>
2021-07-14 12:28:00 -0700

    Remove Mocha and Chai packages and config files

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     
     // From https://jestjs.io/docs/webpack#mocking-css-modules
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/scripts/jestAssetTransformer.js",
         "\\.(css|less)$": "identity-obj-proxy"
     }
 };
