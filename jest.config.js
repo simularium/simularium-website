c1887b23d2434846190f5c61f7d6b67f91eb1de8
Na Hyung Choi <schoinh@gmail.com>
2021-07-13 18:27:48 -0700

    Run all 44 tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
     transform: {
         '^.+\\.(ts|tsx)?$': 'ts-jest',
         "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js",
+        "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
     }
 };
