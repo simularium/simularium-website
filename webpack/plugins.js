3509c682ca8aad9abff144cbe98438b356c0f01f
Na Hyung Choi <schoinh@gmail.com>
2021-07-16 11:00:10 -0700

    Remove plugin import statement

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -6,7 +6,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const webpack = require('webpack');
 
