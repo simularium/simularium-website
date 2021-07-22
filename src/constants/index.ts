0e900ff037437ea7b605f56c9fdc853a5eb8e172
Na Hyung Choi <schoinh@gmail.com>
2021-07-22 15:28:56 -0700

    Change app id from agentviz-ui to simularium-ui

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const APP_ID = "agentviz-ui";
+export const APP_ID = "simularium-ui";
 export const API_VERSION = "v1";
 export const HEADER_HEIGHT = 113;
 export const URL_PARAM_KEY_FILE_NAME = "trajFileName";
