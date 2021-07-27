3c6a4d7b208c36d8f83964afa69387b4fa3c1e7b
meganrm <meganrm@gmail.com>
2021-07-27 11:46:43 -0700

    coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,23 @@ module.exports = {
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/scripts/jestAssetTransformer.js",
         "\\.(css|less)$": "identity-obj-proxy"
+    },
+    coverageThreshold: {
+        "global": {
+            "branches": 50,
+            "functions": 50,
+            "lines": 50,
+            "statements": 50
+        },
+        "./src/state/*/selectors.ts": {
+            // "branches": 80,
+            "lines": 60,
+        },
+        "./src/containers/*/selectors.ts": {
+            "branches": 50,
+            "functions": 50,
+            "lines": 50,
+            "statements": 50
+        }
     }
 };
