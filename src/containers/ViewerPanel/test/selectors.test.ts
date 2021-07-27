9900fbdd68a13cd54e0cfe08e06cadfa51386b8f
meganrm <meganrm@gmail.com>
2021-07-27 12:03:59 -0700

    changed order to match file order, added tests

--- src/containers/ViewerPanel/test/selectors.test.ts
+++ src/containers/ViewerPanel/test/selectors.test.ts
@@ -1,8 +1,86 @@
 import { initialState } from "../../../state/index";
 import { State } from "../../../state/types";
-import { getDisplayTimes, getMaxNumChars } from "../selectors";
+import {
+    convertUIDataToColorMap,
+    getDisplayTimes,
+    getMaxNumChars,
+} from "../selectors";
 
 describe("ViewerPanel selectors", () => {
+    describe("convertUIDataToColorMap", () => {
+        it("converts UI display data to color map where the agent name is the key, and the color is the value", () => {
+            const mockDisplayData = [
+                {
+                    name: "name",
+                    displayStates: [{ name: "state1", id: "id" }],
+                    color: "color",
+                },
+            ];
+            const result = convertUIDataToColorMap(mockDisplayData);
+            expect(result).toEqual({ name: "color" });
+        });
+        it("returns an empty object if no ui data present", () => {
+            const result = convertUIDataToColorMap([]);
+            expect(result).toEqual({});
+        });
+    });
+    describe("getMaxNumChars", () => {
+        it("returns length of lastFrameTime + timeStep when time values are integers", () => {
+            const firstFrameTime = 0;
+            const lastFrameTime = 10;
+            const timeStep = 1;
+
+            const maxNumChars = getMaxNumChars(
+                firstFrameTime,
+                lastFrameTime,
+                timeStep
+            );
+
+            expect(maxNumChars).toBe("11".length);
+        });
+
+        it("returns length of lastFrameTime + timeStep when time values are floats", () => {
+            const firstFrameTime = 0.1;
+            const lastFrameTime = 44.1;
+            const timeStep = 0.8;
+
+            const maxNumChars = getMaxNumChars(
+                firstFrameTime,
+                lastFrameTime,
+                timeStep
+            );
+
+            expect(maxNumChars).toBe("44.9".length);
+        });
+
+        it("returns length of lastFrameTime + timeStep when time values are lengthy floats that need rounding at the end", () => {
+            const firstFrameTime = 0.1;
+            const lastFrameTime = 44.10006;
+            const timeStep = 0.0003;
+
+            const maxNumChars = getMaxNumChars(
+                firstFrameTime,
+                lastFrameTime,
+                timeStep
+            );
+
+            expect(maxNumChars).toBe("44.1".length);
+        });
+
+        it("returns length of firstFrameTime when it is longer than lastFrameTime + timeStep", () => {
+            const firstFrameTime = 0.00001;
+            const lastFrameTime = 44.1;
+            const timeStep = 0.8;
+
+            const maxNumChars = getMaxNumChars(
+                firstFrameTime,
+                lastFrameTime,
+                timeStep
+            );
+
+            expect(maxNumChars).toBe("0.00001".length);
+        });
+    });
     describe("getDisplayTimes", () => {
         it("returns default values when timeUnits doesn't exist", () => {
             const mockState: State = initialState;
@@ -103,64 +181,6 @@ describe("ViewerPanel selectors", () => {
                 roundedTimeStep: 0.08,
                 maxNumChars: "0.000004".length,
             });
-        });
-    });
-
-    describe("getMaxNumChars", () => {
-        it("returns length of lastFrameTime + timeStep when time values are integers", () => {
-            const firstFrameTime = 0;
-            const lastFrameTime = 10;
-            const timeStep = 1;
-
-            const maxNumChars = getMaxNumChars(
-                firstFrameTime,
-                lastFrameTime,
-                timeStep
-            );
-
-            expect(maxNumChars).toBe("11".length);
-        });
-
-        it("returns length of lastFrameTime + timeStep when time values are floats", () => {
-            const firstFrameTime = 0.1;
-            const lastFrameTime = 44.1;
-            const timeStep = 0.8;
-
-            const maxNumChars = getMaxNumChars(
-                firstFrameTime,
-                lastFrameTime,
-                timeStep
-            );
-
-            expect(maxNumChars).toBe("44.9".length);
-        });
-
-        it("returns length of lastFrameTime + timeStep when time values are lengthy floats that need rounding at the end", () => {
-            const firstFrameTime = 0.1;
-            const lastFrameTime = 44.10006;
-            const timeStep = 0.0003;
-
-            const maxNumChars = getMaxNumChars(
-                firstFrameTime,
-                lastFrameTime,
-                timeStep
-            );
-
-            expect(maxNumChars).toBe("44.1".length);
-        });
-
-        it("returns length of firstFrameTime when it is longer than lastFrameTime + timeStep", () => {
-            const firstFrameTime = 0.00001;
-            const lastFrameTime = 44.1;
-            const timeStep = 0.8;
-
-            const maxNumChars = getMaxNumChars(
-                firstFrameTime,
-                lastFrameTime,
-                timeStep
-            );
-
-            expect(maxNumChars).toBe("0.00001".length);
         });
     });
 });
