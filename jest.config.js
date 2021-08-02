b56fd973c77491821cfa3dfbd634be21cb3d98aa
Na Hyung Choi <schoinh@gmail.com>
2021-08-02 16:08:30 -0700

    Add comment in jest config file

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,7 @@ module.exports = {
             "statements": 50
         },
         "./src/state/*/selectors/index.ts": {
+            // TODO: Change to 100%
             "lines": 40,
         },
         "./src/containers/*/selectors.ts": {
