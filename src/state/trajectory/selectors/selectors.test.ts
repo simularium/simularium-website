fac6b885d96f6532edf1a9503d4fa2dd27f0c5a4
Na Hyung Choi <schoinh@gmail.com>
2021-08-03 12:07:21 -0700

    Achieve 100% coverage on trajectory state selectors

--- src/state/trajectory/selectors/selectors.test.ts
+++ src/state/trajectory/selectors/selectors.test.ts
@@ -1,32 +1,18 @@
 import { initialState } from "../../index";
 import { State } from "../../types";
-import { TrajectoryStateBranch } from "../types";
 
-import { getIsNetworkedFile } from ".";
-
-const mockTrajectory: TrajectoryStateBranch = {
-    ...initialState.trajectory,
-    simulariumFile: {
-        name: "",
-        data: [1, 2, 3],
-        lastModified: 2021,
-    },
-};
-const mockState: State = {
-    ...initialState,
-    trajectory: mockTrajectory,
-};
+import { getIsNetworkedFile, getUiDisplayDataTree } from ".";
 
 describe("trajectory composed selectors", () => {
     describe("getIsNetworkedFile", () => {
         it("returns false if there is no file name", () => {
-            expect(getIsNetworkedFile(mockState)).toBe(false);
+            expect(getIsNetworkedFile(initialState)).toBe(false);
         });
         it("returns false if the file name is not found in our list of networked trajectories", () => {
             const state: State = {
-                ...mockState,
+                ...initialState,
                 trajectory: {
-                    ...mockTrajectory,
+                    ...initialState.trajectory,
                     simulariumFile: {
                         name: "Made-up Trajectory",
                         data: [1, 2, 3],
@@ -38,9 +24,9 @@ describe("trajectory composed selectors", () => {
         });
         it("returns true if the file name is found in our list of trajectories and has a title", () => {
             const state: State = {
-                ...mockState,
+                ...initialState,
                 trajectory: {
-                    ...mockTrajectory,
+                    ...initialState.trajectory,
                     simulariumFile: {
                         name: "endocytosis.simularium",
                         data: [1, 2, 3],
@@ -53,6 +39,63 @@ describe("trajectory composed selectors", () => {
     });
 
     describe("getUiDisplayDataTree", () => {
-        it("returns an empty array if ui display data is empty", () => {});
+        it("returns an empty array if ui display data is empty", () => {
+            expect(getUiDisplayDataTree(initialState)).toStrictEqual([]);
+        });
+        it("correctly maps agent display info to an array of display data", () => {
+            const state: State = {
+                ...initialState,
+                trajectory: {
+                    ...initialState.trajectory,
+                    agentUiNames: [
+                        {
+                            name: "agent1",
+                            displayStates: [],
+                        },
+                        {
+                            name: "agent2",
+                            displayStates: [
+                                {
+                                    name: "state1",
+                                    id: "state1_id",
+                                },
+                                {
+                                    name: "state2",
+                                    id: "state2_id",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            };
+
+            const expected = [
+                {
+                    title: "agent1",
+                    key: "agent1",
+                    children: [],
+                },
+                {
+                    title: "agent2",
+                    key: "agent2",
+                    children: [
+                        {
+                            label: "<unmodified>",
+                            value: "",
+                        },
+                        {
+                            label: "state1",
+                            value: "state1_id",
+                        },
+                        {
+                            label: "state2",
+                            value: "state2_id",
+                        },
+                    ],
+                },
+            ];
+
+            expect(getUiDisplayDataTree(state)).toStrictEqual(expected);
+        });
     });
 });
