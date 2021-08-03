265ebb9ec3d1b3e47a4ad090766953007ef1966b
Na Hyung Choi <schoinh@gmail.com>
2021-08-03 11:42:10 -0700

    Make getIsNetworkedFile tests less redundant

--- src/state/trajectory/selectors/selectors.test.ts
+++ src/state/trajectory/selectors/selectors.test.ts
@@ -4,54 +4,55 @@ import { TrajectoryStateBranch } from "../types";
 
 import { getIsNetworkedFile } from ".";
 
+const mockTrajectory: TrajectoryStateBranch = {
+    ...initialState.trajectory,
+    simulariumFile: {
+        name: "",
+        data: [1, 2, 3],
+        lastModified: 2021,
+    },
+};
+const mockState: State = {
+    ...initialState,
+    trajectory: mockTrajectory,
+};
+
 describe("trajectory composed selectors", () => {
     describe("getIsNetworkedFile", () => {
         it("returns false if there is no file name", () => {
-            const mockTrajectory: TrajectoryStateBranch = {
-                ...initialState.trajectory,
-                simulariumFile: {
-                    name: "",
-                    data: [1, 2, 3],
-                    lastModified: 2021,
-                },
-            };
-            const mockState: State = {
-                ...initialState,
-                trajectory: mockTrajectory,
-            };
             expect(getIsNetworkedFile(mockState)).toBe(false);
         });
         it("returns false if the file name is not found in our list of networked trajectories", () => {
-            const mockTrajectory: TrajectoryStateBranch = {
-                ...initialState.trajectory,
-                simulariumFile: {
-                    name: "Made-up Trajectory",
-                    data: [1, 2, 3],
-                    lastModified: 2021,
+            const state: State = {
+                ...mockState,
+                trajectory: {
+                    ...mockTrajectory,
+                    simulariumFile: {
+                        name: "Made-up Trajectory",
+                        data: [1, 2, 3],
+                        lastModified: 2021,
+                    },
                 },
             };
-            const mockState: State = {
-                ...initialState,
-                trajectory: mockTrajectory,
-            };
-            expect(getIsNetworkedFile(mockState)).toBe(false);
+            expect(getIsNetworkedFile(state)).toBe(false);
         });
         it("returns true if the file name is found in our list of trajectories and has a title", () => {
-            const mockTrajectory: TrajectoryStateBranch = {
-                ...initialState.trajectory,
-                simulariumFile: {
-                    name: "endocytosis.simularium",
-                    data: [1, 2, 3],
-                    title: "Endocytosis",
+            const state: State = {
+                ...mockState,
+                trajectory: {
+                    ...mockTrajectory,
+                    simulariumFile: {
+                        name: "endocytosis.simularium",
+                        data: [1, 2, 3],
+                        title: "Endocytosis",
+                    },
                 },
             };
-            const mockState: State = {
-                ...initialState,
-                trajectory: mockTrajectory,
-            };
-            expect(getIsNetworkedFile(mockState)).toBe(true);
+            expect(getIsNetworkedFile(state)).toBe(true);
         });
     });
 
-    describe("getUiDisplayDataTree", () => {});
+    describe("getUiDisplayDataTree", () => {
+        it("returns an empty array if ui display data is empty", () => {});
+    });
 });
