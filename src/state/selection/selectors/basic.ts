88a456f03e86cc8cd6dca43cacab9842f77cb6f5
Na Hyung Choi <schoinh@gmail.com>
2021-08-05 16:41:02 -0700

    Remove unused basic selectors

--- src/state/selection/selectors/basic.ts
+++ src/state/selection/selectors/basic.ts
@@ -1,9 +1,6 @@
 import { State } from "../../types";
 
-export const getSelections = (state: State) => state.selection;
-export const getSelectedFiles = (state: State) => state.selection.files;
 export const getCurrentTime = (state: State) => state.selection.time;
-export const getAgentsOnById = (state: State) => state.selection.agentIdsOn;
 export const getAgentColors = (state: State) => state.selection.agentColors;
 export const getAgentVisibilityMap = (state: State) =>
     state.selection.agentVisibilityMap;
