2ad13cd5f4c3f678c4963d20363be725e79035e0
Na Hyung Choi <schoinh@gmail.com>
2021-08-26 18:13:14 -0700

    Create dummy modal

--- src/components/UrlUpload/index.tsx
+++ src/components/UrlUpload/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Upload, message, Button } from "antd";
+import { Button, Form, Input, Modal } from "antd";
 import { RequestLocalFileAction } from "../../state/trajectory/types";
-import { UploadChangeParam } from "antd/lib/upload";
 
 import { ActionCreator } from "redux";
 import { VIEWER_PATHNAME } from "../../routes";
@@ -13,15 +12,42 @@ interface FileUploadProps {
 const styles = require("./style.css");
 
 const UrlUpload = () => {
+    // TODO: change to false
+    const [isModalVisible, setIsModalVisible] = useState(true);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
     return (
         <Link
             // used to decide whether to clear out the viewer
             to={{
                 pathname: VIEWER_PATHNAME,
-                state: { localFile: true },
+                state: { localFile: true }, // FIXME:
             }}
         >
-            <Button type="ghost">From a URL</Button>
+            <Button type="ghost" onClick={showModal}>
+                From a URL
+            </Button>
+            <Modal
+                title="title"
+                visible={isModalVisible}
+                footer={null}
+                onCancel={handleCancel}
+            >
+                <Form>
+                    <Form.Item>
+                        <Input placeholder="your URL here" allowClear />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary">Submit</Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </Link>
     );
 };
