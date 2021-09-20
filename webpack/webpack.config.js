6f6be1bf0731864479c6a539efceaa0baaebce05
Na Hyung Choi <schoinh@gmail.com>
2021-09-20 15:47:00 -0700

    Rebase to main

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -41,6 +41,7 @@ module.exports = ({ analyze, env } = {}) => ({
                 include: [
                     path.resolve(__dirname, "../", "src/components"),
                     path.resolve(__dirname, "../", "src/containers"),
+                    path.resolve(__dirname, "../", "../simularium-viewer/style"),
                 ],
                 use: [
                     {
