5ffb66a84c046549b83d3bd25c471afafca6a689
Na Hyung Choi <schoinh@gmail.com>
2021-11-08 18:03:46 -0800

    Remove unused bindAll util function and tests

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,21 +1,3 @@
-import { forOwn, isFunction } from "lodash";
-import React from "react";
-
-type AnyFunction = () => any;
-
-export function bindAll<T extends React.Component>(
-    obj: T,
-    methods: AnyFunction[]
-) {
-    const setOfMethods = new Set(methods);
-
-    forOwn(obj.constructor.prototype, (value, key) => {
-        if (setOfMethods.has(value) && isFunction(value)) {
-            Object.assign(obj, { [key]: value.bind(obj) });
-        }
-    });
-}
-
 export function convertToSentenceCase(string: string): string {
     if (!string) {
         return "";
