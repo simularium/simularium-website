313df8ffef5869b64fac742fd82aa3e3ac5e1f99
Na Hyung Choi <schoinh@gmail.com>
2022-02-07 14:08:49 -0800

    Remove number 2 from visual glossary key

--- src/constants/visual-glossary.ts
+++ src/constants/visual-glossary.ts
@@ -7,9 +7,16 @@ const visualGlossary: VisualGlossaryItem[] = [
             "Use this menu to choose an example model to view, or to select a file from a public cloud URL or from your local computer. You can also load a file locally from your computer by dragging it into the viewport.",
     },
     {
-        label: "CLICK TO SELECT & FOLLOW ANY OBJECT",
-        description:
-            "Left mouse clicking on any object in the 3D viewport will trigger the camera to point at it and to move to a helpful viewing distance. The camera will continue to Follow that object (outlined in bright green) until another another is selected, or all objects are deselected by clicking on an empty space in the viewport's background. It's easiest to pause the playback before attempting to CLICK on an object to follow.",
+        label: "CAMERA CONTROLS, from top to bottom:",
+        description: "",
+        bulletItems: [
+            "ROTATE: When this mode is active, click and drag to rotate the view in 3D. This mode is active by default.",
+            "PAN: When this mode is active, click and drag to pan around the view.",
+            "FOCUS: When this mode is active, the selected agent becomes the camera target and gets centered in the view. This mode is active by default.",
+            "ZOOM IN: Move the camera in towards the camera’s current target (either the camera look at position by default, or a followed object).",
+            "ZOOM OUT: Move the camera away from the camera’s target.",
+            "RESET CAMERA: Reset the 3D camera to the default (original) position and orientation.",
+        ],
     },
     {
         label: "CAMERA CONTROLS, from top to bottom:",
