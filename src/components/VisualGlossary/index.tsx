5a88bdd804143915533a515ae2e52041510316f0
Na Hyung Choi <schoinh@gmail.com>
2022-02-15 14:56:45 -0800

    Feature/place visual glossary key next to image on wider screens (#314)
    * Put glossary legend on right side
    
    * Constrain visual glossary to center of screen
    
    * Constrain help text width
    
    * Show key furled out when screen is wide
    
    * Use activeKey prop
    
    * Revert "Use activeKey prop"
    
    This reverts commit 6a58e37841969526af746967728192670e08ea6c.
    
    * Update visual glossary image
    
    * Remove number 2 from visual glossary key
    
    * Revert irrelevant changes
    
    * Update image and glossary key again
    
    * Revert "Revert irrelevant changes"
    
    This reverts commit ec86871d6ed6798769aac8bc1a3431e1c4dd815f.
    
    * Update glossary key dimensions to match new image
    
    * Fix glossary key copy
    
    * Make the key always open when next to image
    
    * Remove unnecessary styling

--- src/components/VisualGlossary/index.tsx
+++ src/components/VisualGlossary/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Collapse } from "antd";
 
 import visualGlossary from "../../constants/visual-glossary";
@@ -26,10 +26,26 @@ const renderGlossaryItems = visualGlossary.map((item: VisualGlossaryItem) => {
 });
 
 const VisualGlossary = (): JSX.Element => {
+    const wideScreenMinWidth = "1622px";
+
+    // Lay out image and legend side-by-side when screen is wide enough
+    const [isScreenWide, setIsScreenWide] = useState(
+        window.matchMedia(`(min-width: ${wideScreenMinWidth})`).matches
+    );
+    useEffect(() => {
+        window
+            .matchMedia(`(min-width: ${wideScreenMinWidth})`)
+            .addEventListener("change", (e) => setIsScreenWide(e.matches));
+    }, []);
+
+    // Legend box should be collapsed by default when underneath image
+    // and always in an open state when next to image
+    const collapseProps = isScreenWide && { activeKey: "1" };
+
     return (
         <div className={styles.container}>
             <img src={visualGlossaryImage} />
-            <Collapse>
+            <Collapse {...collapseProps}>
                 <Collapse.Panel header="Visual Glossary Key" key="1">
                     <ol className={styles.topLevelList}>
                         {renderGlossaryItems}
