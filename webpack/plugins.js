68270129e8db8f0601d110bce3bf311c03ff332b
meganrm <meganrm@gmail.com>
2022-09-19 13:35:20 -0700

    nest gh builds

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -11,28 +11,6 @@ const webpack = require('webpack');
 
 const Env = require('./constants').Env;
 
-const BASE_PLUGINS = [
-    new ForkTsCheckerWebpackPlugin({
-        tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
-        workers: ForkTsCheckerWebpackPlugin.TWO_CPUS_FREE,
-    }),
-    new CleanWebpackPlugin(['dist', 'dev', 'stable'], {
-        root: path.resolve(__dirname, '../'),
-        watch: true,
-    }),
-    new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
-    }),
-    new HtmlWebpackPlugin({
-        favicon: './src/assets/AICS-logo.svg',
-        template: path.resolve(__dirname, 'index.template.html')
-    }),
-    new webpack.EnvironmentPlugin({
-        GH_BUILD: !!process.env.GH_BUILD,
-        GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "AIzaSyAZ3ow-AhfTcOsBml7e3oXZ7JwqIATcGwU"
-    }),
-];
-
 const getBasePlugins = (dist) => {
     return [new ForkTsCheckerWebpackPlugin({
             tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
