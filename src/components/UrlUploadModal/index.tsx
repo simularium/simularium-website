60eada0e8e98251cef0bb3b7d588a7aa616ba4fd
BriteyAC <BritneyArtemis@gmail.com>
2022-10-14 02:05:22 -0700

    fixed potential bug

--- src/components/UrlUploadModal/index.tsx
+++ src/components/UrlUploadModal/index.tsx
@@ -18,9 +18,9 @@ const UrlUploadModal = ({
 
     //focus on input field when modal is opened
     useEffect(() => {
-        const input = document.getElementsByClassName(
-            "ant-input"
-        )[1] as HTMLInputElement;
+        const input = document.getElementsByClassName("ant-input")[
+            document.getElementsByClassName("ant-input").length - 1
+        ] as HTMLInputElement;
         if (input) input.focus();
     }, []);
 
