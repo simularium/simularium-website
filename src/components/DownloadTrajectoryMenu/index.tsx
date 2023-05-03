24aec2a05693f3b005658e54becafdfaf3fd45a3
Joe Heffernan <interim17@gmail.com>
2023-05-03 14:04:03 -0700

    added download functionality

--- src/components/DownloadTrajectoryMenu/index.tsx
+++ src/components/DownloadTrajectoryMenu/index.tsx
@@ -42,10 +42,52 @@ const DownloadTrajectoryMenu = ({
 // setViewerStatus,
 // setError,
 DownloadTrajectoryMenuProps): JSX.Element => {
-    const downloadFile = (data: string, fileName: string): void => {
-        const blob = new Blob([data], { type: "text/plain;charset=utf-8" });
-        const downloadLink = document.createElement("a");
+    const isLocalSimFile = (
+        file: LocalSimFile | NetworkedSimFile
+    ): file is LocalSimFile => {
+        return (file as LocalSimFile).lastModified !== undefined;
+    };
 
+    const fetchFile = async (fileName: string): Promise<string> => {
+        try {
+            const response = await fetch(
+                //JOE QUESTION what URL to use
+                // `URLUNKOWN/download?file={${fileName}.simularium}`
+                `https://aics-simularium-data.s3.us-east-2.amazonaws.com/trajectory/BloodPlasma.simularium`
+                // `https://aics-simularium-data.s3.us-east-2.amazonaws.com/trajectory/{fileName}`
+            );
+            const data = await response.text();
+            return data;
+        } catch {
+            console.log("error fetching file");
+            return "";
+        }
+    };
+
+    const downloadFile = async (fileName: string): Promise<void> => {
+        let blob: Blob | null = new Blob();
+        let data: BlobPart = "";
+
+        if (!isLocalSimFile(simulariumFile)) {
+            console.log("downloading networked file");
+            try {
+                data = await fetchFile(simulariumFile.name);
+                //JOE QUESTION: what is the appropriate type for the blob?
+                blob = new Blob([data], {
+                    type: "text/plain;charset=utf-8",
+                });
+            } catch {
+                console.log("error downloading file");
+            }
+        }
+        // if we have a local file, we can just download it, but how to make sure that we capture viewer state and selection?
+        else {
+            data = JSON.stringify(simulariumFile.data);
+            blob = new Blob([data.slice(18, -1)], {
+                type: "text/plain;charset=utf-8",
+            });
+        }
+        const downloadLink = document.createElement("a");
         downloadLink.href = URL.createObjectURL(blob);
         downloadLink.download = fileName;
         downloadLink.style.display = "none";
@@ -57,10 +99,8 @@ DownloadTrajectoryMenuProps): JSX.Element => {
     };
 
     const onClick = () => {
-        console.log(typeof simulariumFile);
-        console.log("sim file data", simulariumFile.data);
         console.log("sim file name", simulariumFile.name);
-        downloadFile(simulariumFile.data, simulariumFile.name);
+        downloadFile(simulariumFile.name);
     };
 
     return (
