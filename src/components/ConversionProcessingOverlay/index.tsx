7391d54e74b90e7b0c39e31f771f5fab8641c582
meganrm <meganrm@gmail.com>
2023-07-10 16:30:45 -0700

    consolidate css

--- src/components/ConversionProcessingOverlay/index.tsx
+++ src/components/ConversionProcessingOverlay/index.tsx
@@ -29,40 +29,39 @@ const ConversionProcessingOverlay = ({
                     cancelProcessing={toggleProcessing}
                 />
             ) : null}
-            <h2 className={styles.titleText}> File conversion in progress </h2>
-            <p className={styles.centerText}>
+            <h2 className={styles.title}> File conversion in progress </h2>
+            <div className={styles.content}>
                 {" "}
                 {fileName} is being converted and will load when complete.{" "}
                 <br></br>
                 Processing time will vary depending on file size.
-            </p>
-            <a
-                className={styles.goBackButton}
-                onClick={() => setCancelModalOpen(true)}
-            >
-                {" "}
-                {LeftArrow} Stop and go back to form{" "}
-            </a>
-            <div className={styles.spin}>
-                <Spin size={"large"}> </Spin>
+                <a
+                    className={styles.goBackButton}
+                    onClick={() => setCancelModalOpen(true)}
+                >
+                    {" "}
+                    {LeftArrow} Stop and go back to form{" "}
+                </a>
+                <div className={styles.processingIndicator}>
+                    <div className={styles.spinContainer}>
+                        <Spin size={"large"}> </Spin>
+                    </div>
+                    <div>Processing...</div>
+                </div>
+                <h3>Open another instance of Simularium </h3>
+                <Button
+                    className={styles.button}
+                    href={`https://simularium.allencell.org/viewer`}
+                    target="_blank"
+                >
+                    Open in new tab {UpRightArrow}
+                </Button>
             </div>
-            <p className={styles.processingMargin}> Processing... </p>
-            <h3 className={styles.mediumText}>
-                {" "}
-                Open another instance of Simularium{" "}
-            </h3>
-            <Button
-                className={styles.tabButton}
-                href={`https://simularium.allencell.org/viewer`}
-                target="_blank"
-            >
-                Open in new tab {UpRightArrow}
-            </Button>
             <div className={styles.dividerContainer}>
                 <Divider> </Divider>
             </div>
             <Button
-                className={styles.cancelButton}
+                className={styles.button}
                 onClick={() => setCancelModalOpen(true)}
             >
                 Cancel file import
