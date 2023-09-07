7190b432120d3fb9e973d5761ed6afc7389aa6a6
Joe Heffernan <interim17@gmail.com>
2023-09-07 16:27:56 -0700

    add colorPicker to color swatch

--- src/components/ColorSwatch/index.tsx
+++ src/components/ColorSwatch/index.tsx
@@ -1,14 +1,45 @@
 import * as React from "react";
 
 import styles from "./styles.css";
-
+import OurColorPicker from "../ColorPickerModal";
+import { useEffect, useState } from "react";
 interface ColorSwatchProps {
     color: string;
+    agentName: string;
+    tags: string[];
 }
 
-const ColorSwatch = ({ color }: ColorSwatchProps): JSX.Element => {
+const ColorSwatch = ({
+    color,
+    agentName,
+    tags,
+}: ColorSwatchProps): JSX.Element => {
+    const [isColorPickerVisible, setColorPickerVisible] = React.useState(false);
+    const [newColor, setNewColor] = useState(color);
+
+    const closeModal = () => {
+        setColorPickerVisible(false);
+    };
+
     return (
-        <div className={styles.container} style={{ backgroundColor: color }} />
+        <>
+            <div
+                className={styles.container}
+                style={{ backgroundColor: newColor }}
+                onClick={() => {
+                    setColorPickerVisible(true);
+                }}
+            />
+            {isColorPickerVisible ? (
+                <OurColorPicker
+                    agentName={agentName}
+                    tags={tags}
+                    oldColor={color}
+                    isOpen={isColorPickerVisible}
+                    closeModal={closeModal}
+                />
+            ) : null}
+        </>
     );
 };
 
