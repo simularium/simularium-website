bcf457f7dc730fc2b7d190c561209a28328f4d10
Joe Heffernan <interim17@gmail.com>
2023-09-07 16:26:37 -0700

    pass props and helders to color picker modal

--- src/components/ColorPickerModal/index.tsx
+++ src/components/ColorPickerModal/index.tsx
@@ -4,22 +4,38 @@ import ColorPicker from "../ColorPicker";
 
 import styles from "./style.css";
 
-const OurColorPicker = () => {
-    const [color, setColor] = useState({ h: 250, s: 0.5, l: 0.5, a: 1 });
+interface OurColorPickerProps {
+    oldColor: string;
+    isOpen: boolean;
+    closeModal: () => void;
+    agentName: string;
+    tags: string[];
+}
 
+const OurColorPicker = ({
+    oldColor,
+    isOpen,
+    closeModal,
+    agentName,
+    tags,
+}: OurColorPickerProps) => {
     return (
         <Modal
             className={styles.modalStyle}
-            open={true}
+            open={isOpen}
             title=""
             footer={null}
-            closeIcon={false}
-            closable={false}
+            closeIcon={true}
+            closable={true}
             width={225}
+            onCancel={closeModal}
         >
-            <ColorPicker />
+            <ColorPicker
+                agentName={agentName}
+                tags={tags}
+                oldColor={oldColor}
+            />
         </Modal>
-        //  </div>
     );
 };
 export default OurColorPicker;
