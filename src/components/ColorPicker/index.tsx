53d677fbf97e947cca666716f43083300ece6798
Joe Heffernan <interim17@gmail.com>
2023-09-07 16:25:44 -0700

    handleColorChange in picker

--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,33 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HexColorInput, HexColorPicker } from "react-colorful";
+import { ActionCreator } from "redux";
 import styles from "./style.css";
 import classNames from "classnames";
 
-// import { AGENT_COLORS } from "../../containers/ViewerPanel/constants";
+import selectionStateBranch from "../../state/selection";
+import { AGENT_COLORS } from "../../containers/ViewerPanel/constants";
+import {
+    ColorChangesMap,
+    SetColorChangesAction,
+} from "../../state/selection/types";
+import { connect } from "react-redux";
 
-const ColorPicker = () => {
-    const [color, setColor] = useState("#b32aa9");
+interface ColorPickerProps {
+    oldColor: string;
+    agentName: string;
+    tags: string[];
+    setColorChanges: ActionCreator<SetColorChangesAction>;
+}
 
-    const agentColors = [
-        "#fee34d",
-        "#f7b232",
-        "#bf5736",
-        "#94a7fc",
-        "#ce8ec9",
-        "#58606c",
-        "#0ba345",
-        "#9267cb",
-        "#81dbe6",
-        "#94a7fc",
-        "#ce8ec9",
-        "#58606c",
-        "#0ba345",
-        "#9267cb",
-        "#81dbe6",
-        "#0ba345",
-        "#9267cb",
-        "#81dbe6",
-    ];
+const ColorPicker = ({
+    oldColor,
+    setColorChanges,
+    agentName,
+    tags,
+}: ColorPickerProps) => {
+    const [color, setColor] = useState(oldColor);
+
+    const handleColorChange = (color: string) => {
+        const colorChanges: ColorChangesMap = {
+            agents: { [agentName]: tags },
+            color: color,
+        };
+        console.log("colorChanges", colorChanges);
+        console.log("setColorChanges", setColorChanges);
+        setColorChanges(colorChanges);
+    };
+
+    useEffect(() => {
+        handleColorChange(color);
+    }, [color]);
 
     const recentColors = [
         "#bd7800",
@@ -46,19 +58,29 @@ const ColorPicker = () => {
         "#81dbe6",
     ];
 
-    const blue = "#0000ff";
-    const red = "#ff0000";
-
     return (
         <div className={styles.container}>
             <HexColorPicker color={color} onChange={setColor} />
             <div className={styles.selectionDisplay}>
                 <div className={styles.oldColorContainer}>
-                    <div className={styles.oldColor}> </div>
+                    <div
+                        className={styles.oldColor}
+                        style={{ backgroundColor: oldColor }}
+                        onClick={() => {
+                            setColor(oldColor);
+                        }}
+                    >
+                        {" "}
+                    </div>
                     <p> CURRENT </p>
                 </div>
                 <div className={styles.oldColorContainer}>
-                    <div className={styles.newColor}> </div>
+                    <div
+                        className={styles.newColor}
+                        style={{ backgroundColor: color }}
+                    >
+                        {" "}
+                    </div>
                     <p> NEW </p>
                 </div>
                 <div className={styles.oldColorContainer}>
@@ -76,7 +98,7 @@ const ColorPicker = () => {
                     styles.agentColorsSwatches,
                 ])}
             >
-                {agentColors.map((color) => (
+                {AGENT_COLORS.map((color) => (
                     <button
                         key={color}
                         className={classNames([
@@ -107,4 +129,8 @@ const ColorPicker = () => {
     );
 };
 
-export default ColorPicker;
+const dispatchToPropsMap = {
+    setColorChanges: selectionStateBranch.actions.setColorChanges,
+};
+
+export default connect(null, dispatchToPropsMap)(ColorPicker);
