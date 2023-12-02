9386701363ba99e7782e74d2c9867abaef942dfd
Joe Heffernan <interim17@gmail.com>
2023-12-01 18:00:44 -0800

    send server health checks with timeouts

--- src/containers/ConversionForm/index.tsx
+++ src/containers/ConversionForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Upload, Select, Divider, Button } from "antd";
 import classNames from "classnames";
 import { ActionCreator } from "redux";
@@ -28,6 +28,7 @@ import ConversionServerCheckModal from "../../components/ConversionServerCheckMo
 import { SimulariumController } from "@aics/simularium-viewer";
 import ConversionProcessingOverlay from "../../components/ConversionProcessingOverlay";
 import ConversionFileErrorModal from "../../components/ConversionFileErrorModal";
+import { NetConnectionParams } from "@aics/simularium-viewer/type-declarations/simularium";
 
 interface ConversionProps {
     setConversionEngine: ActionCreator<SetConversionEngineAction>;
@@ -40,7 +41,6 @@ interface ConversionProps {
     receiveFileToConvert: ActionCreator<ReceiveFileToConvertAction>;
     setError: ActionCreator<SetErrorAction>;
     simulariumController: SimulariumController;
-    serverHealth: boolean;
 }
 
 const validFileExtensions: ExtensionMap = {
@@ -66,8 +66,7 @@ const ConversionForm = ({
     conversionProcessingData,
     setError,
     receiveFileToConvert,
-    // simulariumController,
-    serverHealth,
+    simulariumController,
 }: ConversionProps): JSX.Element => {
     const [fileToConvert, setFileToConvert] = useState<UploadFile>();
     const [engineSelected, setEngineSelected] = useState<boolean>(false);
@@ -75,6 +74,71 @@ const ConversionForm = ({
         useState<boolean>(false);
     const [isProcessing, setIsProcessing] = useState<boolean>(false);
     const [fileTypeErrorModalOpen, setFileTypeErrorModalOpen] = useState(false);
+    const [serverHealth, setServerHealth] = useState<boolean>(false);
+
+    // possible to arrive here without configuring a controller
+    // so we need this config to send a health check
+    const netConnectionConfig: NetConnectionParams = {
+        serverIp: "0.0.0.0",
+        serverPort: 8765,
+        useOctopus: true,
+        secureConnection: false,
+    };
+    const controller =
+        simulariumController ||
+        new SimulariumController({
+            netConnectionSettings: netConnectionConfig,
+        });
+
+    const generateRequestId = () => {
+        return Date.now(); // simple example, you might want something more sophisticated
+    };
+
+    const healthCheckTimeouts: any = {};
+
+    const onHealthCheckResponse = (requestId: any): void => {
+        setServerHealth(true);
+
+        // Clear the timeout if a response is received
+        if (healthCheckTimeouts[requestId]) {
+            clearTimeout(healthCheckTimeouts[requestId]);
+            delete healthCheckTimeouts[requestId]; // Clean up the timeout reference
+        }
+    };
+
+    const checkServerHealthWithTimeout = (
+        netConnectionConfig: NetConnectionParams
+    ) => {
+        const requestId = generateRequestId();
+        controller.checkServerHealth(
+            () => onHealthCheckResponse(requestId),
+            netConnectionConfig
+        );
+
+        // Set a 15-second timeout for this health check request
+        healthCheckTimeouts[requestId] = setTimeout(() => {
+            if (healthCheckTimeouts[requestId]) {
+                setServerHealth(false);
+                delete healthCheckTimeouts[requestId]; // Clean up the timeout reference
+            }
+        }, 15000);
+    };
+
+    // configure controller and send health check
+    useEffect(() => {
+        checkServerHealthWithTimeout(netConnectionConfig);
+    }, []);
+
+    // useEffect to log a change in server health
+    useEffect(() => {
+        console.log("we received a server health response! :)");
+        console.log(
+            "localServerHealth",
+            serverHealth,
+            "netConnectionConfig",
+            netConnectionConfig
+        );
+    }, [serverHealth]);
 
     const toggleServerCheckModal = () => {
         setServerIsDownModalOpen(!serverDownModalOpen);
@@ -95,18 +159,9 @@ const ConversionForm = ({
 
     const handleFileSelection = async (file: UploadFile) => {
         setFileToConvert(file);
-        // necessarry to check if simController exists?
-        // if server is down, viewer state will update and trigger modal
-        // below method doesn't exist yet:
-        // simulariumController.sendServerCheck();
-
-        // to simulate server being down when we upload:
         if (!serverHealth) {
             setServerIsDownModalOpen(true);
         }
-
-        // toggle serverDown so that modal will close
-        // but unless serverHealth changes, Next button will be disabled
     };
 
     const validateFileType = (fileName: string) => {
@@ -117,11 +172,17 @@ const ConversionForm = ({
                 fileExtension.toLowerCase()
             ) {
                 setIsProcessing(!isProcessing);
-                return;
+                return true;
             }
         }
-
         setFileTypeErrorModalOpen(true);
+        return false;
+    };
+
+    const doTheNextThing = () => {
+        console.log();
+        // this should ping server
+        checkServerHealthWithTimeout(netConnectionConfig);
     };
 
     // TODO: use conversion template data to render the form
@@ -199,9 +260,14 @@ const ConversionForm = ({
                 <Button
                     type="primary"
                     disabled={!readyToConvert}
-                    onClick={() =>
-                        readyToConvert && validateFileType(fileToConvert.name)
-                    } // this will be unclickable anyway, but typescript doesn't know that
+                    onClick={() => {
+                        if (
+                            readyToConvert &&
+                            validateFileType(fileToConvert.name)
+                        ) {
+                            doTheNextThing();
+                        }
+                    }} // this will be unclickable anyway, but typescript doesn't know that
                 >
                     Next
                 </Button>
@@ -218,7 +284,6 @@ function mapStateToProps(state: State) {
             trajectoryStateBranch.selectors.getConversionProcessingData(state),
         simulariumController:
             simulariumStateBranch.selectors.getSimulariumController(state),
-        serverHealth: viewerStateBranch.selectors.getServerHealth(state),
     };
 }
 
@@ -226,6 +291,8 @@ const dispatchToPropsMap = {
     receiveFileToConvert: trajectoryStateBranch.actions.receiveFileToConvert,
     setError: viewerStateBranch.actions.setError,
     setConversionEngine: trajectoryStateBranch.actions.setConversionEngine,
+    setSimulariumController:
+        simulariumStateBranch.actions.setSimulariumController,
 };
 
 export default connect(mapStateToProps, dispatchToPropsMap)(ConversionForm);
