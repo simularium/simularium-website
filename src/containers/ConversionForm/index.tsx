759c79a31336f773ea12ecf213177a256c9d3784
Joe Heffernan <interim17@gmail.com>
2023-12-04 12:49:01 -0800

    adds interval checks and helper functions

--- src/containers/ConversionForm/index.tsx
+++ src/containers/ConversionForm/index.tsx
@@ -30,6 +30,11 @@ import ConversionProcessingOverlay from "../../components/ConversionProcessingOv
 import ConversionFileErrorModal from "../../components/ConversionFileErrorModal";
 import { NetConnectionParams } from "@aics/simularium-viewer/type-declarations/simularium";
 
+// TODO QUESTIONS
+// Currently sending one request on page load and another request on file upload... sufficient?
+// I'm using Date.now() to generate a request ID, there are libraries like nanoid or uuid but i dont want to add unneeded dependencies
+// 15 second time out for health check responses, is that too long?
+
 interface ConversionProps {
     setConversionEngine: ActionCreator<SetConversionEngineAction>;
     conversionProcessingData: {
@@ -41,6 +46,10 @@ interface ConversionProps {
     receiveFileToConvert: ActionCreator<ReceiveFileToConvertAction>;
     setError: ActionCreator<SetErrorAction>;
     simulariumController: SimulariumController;
+}
+
+interface HealthCheckTimeouts {
+    [requestId: number]: NodeJS.Timeout;
 }
 
 const validFileExtensions: ExtensionMap = {
@@ -61,6 +70,15 @@ const selectOptions = Object.keys(AvailableEngines).map(
     }
 );
 
+// possible to arrive here without configuring a controller
+// so we need this config to send a health check
+const netConnectionConfig: NetConnectionParams = {
+    serverIp: "0.0.0.0",
+    serverPort: 8765,
+    useOctopus: true,
+    secureConnection: false,
+};
+
 const ConversionForm = ({
     setConversionEngine,
     conversionProcessingData,
@@ -68,70 +86,83 @@ const ConversionForm = ({
     receiveFileToConvert,
     simulariumController,
 }: ConversionProps): JSX.Element => {
-    const [fileToConvert, setFileToConvert] = useState<UploadFile>();
+    const [serverHealth, setServerHealth] = useState<boolean>(false);
     const [engineSelected, setEngineSelected] = useState<boolean>(false);
+    const [fileToConvert, setFileToConvert] = useState<UploadFile>();
     const [serverDownModalOpen, setServerIsDownModalOpen] =
         useState<boolean>(false);
-    const [isProcessing, setIsProcessing] = useState<boolean>(false);
     const [fileTypeErrorModalOpen, setFileTypeErrorModalOpen] = useState(false);
-    const [serverHealth, setServerHealth] = useState<boolean>(false);
+    const [isProcessing, setIsProcessing] = useState<boolean>(false);
 
-    // possible to arrive here without configuring a controller
-    // so we need this config to send a health check
-    const netConnectionConfig: NetConnectionParams = {
-        serverIp: "0.0.0.0",
-        serverPort: 8765,
-        useOctopus: true,
-        secureConnection: false,
-    };
+    const readyToConvert = fileToConvert && engineSelected;
+
+    // TODO is it necessary to check if existing controller is not RemoteSimulator?
     const controller =
         simulariumController ||
         new SimulariumController({
             netConnectionSettings: netConnectionConfig,
         });
 
-    const generateRequestId = () => {
-        return Date.now(); // simple example, you might want something more sophisticated
-    };
+    const healthCheckTimeouts: HealthCheckTimeouts = {};
+    let healthCheckInterval: NodeJS.Timeout | null = null;
 
-    const healthCheckTimeouts: any = {};
-
-    const onHealthCheckResponse = (requestId: any): void => {
-        setServerHealth(true);
-
-        // Clear the timeout if a response is received
-        if (healthCheckTimeouts[requestId]) {
-            clearTimeout(healthCheckTimeouts[requestId]);
-            delete healthCheckTimeouts[requestId]; // Clean up the timeout reference
+    // will use this interval to send health checks every 15 seconds if one fails
+    const setHealthCheckInterval = () => {
+        if (healthCheckInterval) {
+            clearInterval(healthCheckInterval);
         }
+        healthCheckInterval = setInterval(sendHealthCheck, 15000);
     };
 
-    const checkServerHealthWithTimeout = (
-        netConnectionConfig: NetConnectionParams
-    ) => {
+    const generateRequestId = () => {
+        return Date.now();
+    };
+
+    // sends a health check and sets initial 15 second timeout
+    const sendHealthCheck = () => {
         const requestId = generateRequestId();
         controller.checkServerHealth(
             () => onHealthCheckResponse(requestId),
             netConnectionConfig
         );
 
-        // Set a 15-second timeout for this health check request
         healthCheckTimeouts[requestId] = setTimeout(() => {
+            // no response received in time
             if (healthCheckTimeouts[requestId]) {
                 setServerHealth(false);
-                delete healthCheckTimeouts[requestId]; // Clean up the timeout reference
+                delete healthCheckTimeouts[requestId];
+                // Start interval checks only if not already started
+                if (!healthCheckInterval) {
+                    setHealthCheckInterval();
+                }
             }
         }, 15000);
     };
 
-    // configure controller and send health check
+    // callback for viewer
+    // sets server health state, clears timeouts and intervals
+    const onHealthCheckResponse = (requestId: number): void => {
+        setServerHealth(true);
+
+        if (healthCheckTimeouts[requestId]) {
+            clearTimeout(healthCheckTimeouts[requestId]);
+            delete healthCheckTimeouts[requestId];
+        }
+
+        // Stop the interval checks as server is healthy
+        if (healthCheckInterval) {
+            clearInterval(healthCheckInterval);
+            healthCheckInterval = null;
+        }
+    };
+
+    // On load, configure controller and send request
     useEffect(() => {
-        checkServerHealthWithTimeout(netConnectionConfig);
+        sendHealthCheck();
     }, []);
 
     // useEffect to log a change in server health
     useEffect(() => {
-        console.log("we received a server health response! :)");
         console.log(
             "localServerHealth",
             serverHealth,
@@ -140,6 +171,7 @@ const ConversionForm = ({
         );
     }, [serverHealth]);
 
+    // callback for modal to use
     const toggleServerCheckModal = () => {
         setServerIsDownModalOpen(!serverDownModalOpen);
     };
@@ -157,11 +189,11 @@ const ConversionForm = ({
         setEngineSelected(true);
     };
 
+    // we sent one health check on page load
+    // that might have been a while ago, lets send another
     const handleFileSelection = async (file: UploadFile) => {
+        sendHealthCheck();
         setFileToConvert(file);
-        if (!serverHealth) {
-            setServerIsDownModalOpen(true);
-        }
     };
 
     const validateFileType = (fileName: string) => {
@@ -171,7 +203,6 @@ const ConversionForm = ({
                 validFileExtensions[conversionProcessingData.engineType] ===
                 fileExtension.toLowerCase()
             ) {
-                setIsProcessing(!isProcessing);
                 return true;
             }
         }
@@ -179,15 +210,20 @@ const ConversionForm = ({
         return false;
     };
 
-    const doTheNextThing = () => {
-        console.log();
-        // this should ping server
-        checkServerHealthWithTimeout(netConnectionConfig);
+    const advanceIfServerIsHealthy = () => {
+        if (readyToConvert && validateFileType(fileToConvert.name)) {
+            if (!serverHealth) {
+                setServerIsDownModalOpen(true);
+            } else {
+                // at this point: engine selected, file uploaded, file type valid, server health received
+                setIsProcessing(true);
+            }
+        }
     };
 
     // TODO: use conversion template data to render the form
     console.log("conversion form data", conversionProcessingData);
-    const readyToConvert = fileToConvert && engineSelected && serverHealth;
+
     const conversionForm = (
         <div className={classNames(styles.container, theme.lightTheme)}>
             {serverDownModalOpen && (
@@ -260,14 +296,7 @@ const ConversionForm = ({
                 <Button
                     type="primary"
                     disabled={!readyToConvert}
-                    onClick={() => {
-                        if (
-                            readyToConvert &&
-                            validateFileType(fileToConvert.name)
-                        ) {
-                            doTheNextThing();
-                        }
-                    }} // this will be unclickable anyway, but typescript doesn't know that
+                    onClick={advanceIfServerIsHealthy}
                 >
                     Next
                 </Button>
