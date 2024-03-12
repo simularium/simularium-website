50a8c6f256a0432849ce6c731bed26c1293b123f
Joe Heffernan <interim17@gmail.com>
2024-03-12 10:43:06 -0700

    fix: organize colors, modal buttons (#473)
    * organize and prune color variables and apply light theme at custom modal level
    
    * use var for disabled input text color
    
    * prevent flicker on select file button
    
    * remove classnames import from file upload modal
    
    * restore necessry whitespace in version modal text
    
    * position close x correctly in modal header
    
    * fix typo in focus selector
    
    * remove unneeded ant animation prop

--- src/components/CustomModal/index.tsx
+++ src/components/CustomModal/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Modal, ModalProps } from "antd";
 import classNames from "classnames";
 
+import theme from "../theme/light-theme.css";
 import styles from "./style.css";
 
 /** Tiny wrapper component to keep modal styling consistent */
 const CustomModal: React.FC<ModalProps> = (props) => (
-    <Modal {...props} className={classNames(styles.modal, props.className)} />
+    <Modal
+        {...props}
+        className={classNames(theme.lightTheme, styles.modal, props.className)}
+    />
 );
 
 export default CustomModal;
