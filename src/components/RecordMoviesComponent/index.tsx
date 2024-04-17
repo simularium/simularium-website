f48f6acb4f45b91bfe69ebd44091ee603c12716b
Joe Heffernan <interim17@gmail.com>
2024-04-17 14:48:45 -0700

    remove unsed comment

--- src/components/RecordMoviesComponent/index.tsx
+++ src/components/RecordMoviesComponent/index.tsx
@@ -94,33 +94,6 @@ const RecordMovieComponent = (props: RecordMovieComponentProps) => {
         return "Start movie recording";
     };
 
-    // /**
-    //  * onloadedmetadata is async hence the need for this custom hook
-    //  * to retrieve the duration from the recorded movie.
-    //  */
-    // export const useFormattedDuration = (movieUrl: string) => {
-    //     const [formattedDuration, setFormattedDuration] = useState("");
-
-    //     useEffect(() => {
-    //         const video = document.createElement("video");
-    //         video.src = movieUrl;
-    //         video.onloadedmetadata = () => {
-    //             const durationInSeconds = video.duration;
-    //             const minutes = Math.floor((durationInSeconds % 3600) / 60);
-    //             const secs = Math.ceil(durationInSeconds) % 60;
-
-    //             const minutesStr = minutes.toString().padStart(2, "0");
-    //             const secondsStr = secs.toString().padStart(2, "0");
-
-    //             setFormattedDuration(`${minutesStr}:${secondsStr}`);
-    //             video.src = "";
-    //             video.load();
-    //         };
-    //     }, [movieUrl]);
-
-    //     return formattedDuration;
-    // };
-
     const duration = useFormattedDuration(movieUrl);
 
     return (
