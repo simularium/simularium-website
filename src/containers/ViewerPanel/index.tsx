a9280a96da338c07283342a760caf63b28f7af77
Joe Heffernan <interim17@gmail.com>
2024-04-23 16:39:31 -0700

    Feature/record movie UI (#483)
    * use custom modal wrapper for more layout and styling
    
    * remove duplicate and unused colors
    
    * remove more duplicated colors
    
    * remove comments
    
    * remove unused import from starcheckbox
    
    * simplify time input css selector
    
    * implement record movies ui
    
    * only store video url in state, get duration in record movies compennt and title in download handler
    
    * move download function into record movie component
    
    * getMovieTitle can be a private method
    
    * Squashed commit of the following:
    
    commit f622cae8e76523134da56e358dcaa67546ed7f85
    Author: Joe Heffernan <interim17@gmail.com>
    Date:   Tue Apr 2 09:12:34 2024 -0700
    
        Fix/update custom modal (#482)
    
        * use custom modal wrapper for more layout and styling
    
        * remove duplicate and unused colors
    
        * remove more duplicated colors
    
        * remove comments
    
        * remove unused import from starcheckbox
    
        * simplify time input css selector
    
        * fix spelling errors in "shareable"
    
        * omit unused ModalProps from CustomModalProps
    
        * add comment to share modal input styles
    
    commit 0af4198e08cf4db36edf13f9d99e28323384e1c5
    Author: Joe Heffernan <interim17@gmail.com>
    Date:   Mon Apr 1 09:48:40 2024 -0700
    
        Fix/header buttons (#484)
    
        * custom nav button component for header buttons
    
        * remove tooltip button stylesheet
    
        * make header a functional component and implement nav buttons
    
        * remove animation on home button
    
        * remove unneeded style sheets and class names from action button components
    
        * use interface for tooltip text
    
        * remove unused import
    
        * delete unused stylesheet
    
        * move tooltip placement type to constants
    
        * default nav buttons to action styling and trim selectors
    
        * provide default disabled tooltip text and simplify conditional checks
    
    commit f527adb8a07712262dd681b68f65590bb5e8cade
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Thu Mar 28 11:16:17 2024 -0700
    
        Bump webpack-dev-middleware from 5.3.3 to 5.3.4 (#481)
    
        Bumps [webpack-dev-middleware](https://github.com/webpack/webpack-dev-middleware) from 5.3.3 to 5.3.4.
        - [Release notes](https://github.com/webpack/webpack-dev-middleware/releases)
        - [Changelog](https://github.com/webpack/webpack-dev-middleware/blob/v5.3.4/CHANGELOG.md)
        - [Commits](https://github.com/webpack/webpack-dev-middleware/compare/v5.3.3...v5.3.4)
    
        ---
        updated-dependencies:
        - dependency-name: webpack-dev-middleware
          dependency-type: indirect
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>
        Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * remove unused variables
    
    * organization and clarifying comments
    
    * use single function to get icon in record movies component
    
    * use selector to get movie title
    
    * rename cleanup movie state to resetAfterMovieRecording
    
    * move useFormattedDuration hook into its own file
    
    * remove unsed comment
    
    * add test for getMovieTitle
    
    * remove unused import

--- src/containers/ViewerPanel/index.tsx
+++ src/containers/ViewerPanel/index.tsx
@@ -43,6 +43,7 @@ import {
 } from "../../state/trajectory/types";
 import { batchActions } from "../../state/util";
 import PlaybackControls from "../../components/PlaybackControls";
+import RecordMoviesComponent from "../../components/RecordMoviesComponent";
 import CameraControls from "../../components/CameraControls";
 import ScaleBar from "../../components/ScaleBar";
 import { TUTORIAL_PATHNAME } from "../../routes";
@@ -52,6 +53,7 @@ import {
     convertUIDataToSelectionData,
     getDisplayTimes,
     getSelectionStateInfoForViewer,
+    getMovieTitle,
 } from "./selectors";
 import { AGENT_COLORS } from "./constants";
 import { DisplayTimes } from "./types";
@@ -92,6 +94,7 @@ interface ViewerPanelProps {
     error: ViewerError;
     setBuffering: ActionCreator<ToggleAction>;
     setError: ActionCreator<SetErrorAction>;
+    movieTitle: string;
 }
 
 interface ViewerPanelState {
@@ -99,6 +102,7 @@ interface ViewerPanelState {
     particleTypeIds: string[];
     height: number;
     width: number;
+    movieURL: string;
 }
 
 class ViewerPanel extends React.Component<ViewerPanelProps, ViewerPanelState> {
@@ -123,6 +127,7 @@ class ViewerPanel extends React.Component<ViewerPanelProps, ViewerPanelState> {
             particleTypeIds: [],
             height: 0,
             width: 0,
+            movieURL: "",
         };
     }
 
@@ -364,6 +369,22 @@ class ViewerPanel extends React.Component<ViewerPanelProps, ViewerPanelState> {
         }
     };
 
+    public resetAfterMovieRecording = () => {
+        if (this.state.movieURL) {
+            URL.revokeObjectURL(this.state.movieURL);
+        }
+        this.setState({
+            movieURL: "",
+        });
+    };
+
+    public onRecordedMovie = (videoBlob: Blob) => {
+        const url = URL.createObjectURL(videoBlob);
+        this.setState({
+            movieURL: url,
+        });
+    };
+
     public render(): JSX.Element {
         const {
             time,
@@ -381,6 +402,7 @@ class ViewerPanel extends React.Component<ViewerPanelProps, ViewerPanelState> {
             status,
             setError,
             scaleBarLabel,
+            movieTitle,
         } = this.props;
         return (
             <div
@@ -413,27 +435,40 @@ class ViewerPanel extends React.Component<ViewerPanelProps, ViewerPanelState> {
                     onTrajectoryFileInfoChanged={
                         this.onTrajectoryFileInfoChanged
                     }
+                    onRecordedMovie={this.onRecordedMovie}
                 />
                 {firstFrameTime !== lastFrameTime && (
-                    <PlaybackControls
-                        playHandler={this.startPlay}
-                        time={time}
-                        timeStep={timeStep}
-                        displayTimes={displayTimes}
-                        timeUnits={timeUnits}
-                        onTimeChange={this.skipToTime}
-                        pauseHandler={this.pause}
-                        prevHandler={this.playBackOne}
-                        nextHandler={this.playForwardOne}
-                        isPlaying={isPlaying}
-                        isLooping={isLooping}
-                        loopHandler={this.toggleLooping}
-                        firstFrameTime={firstFrameTime}
-                        lastFrameTime={lastFrameTime}
-                        loading={isBuffering}
-                        isEmpty={status === VIEWER_EMPTY}
-                    />
+                    <div className={styles.bottomControlsContainer}>
+                        <PlaybackControls
+                            playHandler={this.startPlay}
+                            time={time}
+                            timeStep={timeStep}
+                            displayTimes={displayTimes}
+                            timeUnits={timeUnits}
+                            onTimeChange={this.skipToTime}
+                            pauseHandler={this.pause}
+                            prevHandler={this.playBackOne}
+                            nextHandler={this.playForwardOne}
+                            isPlaying={isPlaying}
+                            isLooping={isLooping}
+                            loopHandler={this.toggleLooping}
+                            firstFrameTime={firstFrameTime}
+                            lastFrameTime={lastFrameTime}
+                            loading={isBuffering}
+                            isEmpty={status === VIEWER_EMPTY}
+                        />
+                        <RecordMoviesComponent
+                            movieUrl={this.state.movieURL}
+                            movieTitle={movieTitle}
+                            resetAfterMovieRecording={
+                                this.resetAfterMovieRecording
+                            }
+                            startRecording={simulariumController.startRecording}
+                            stopRecording={simulariumController.stopRecording}
+                        />
+                    </div>
                 )}
+
                 <ScaleBar label={scaleBarLabel} />
                 <CameraControls
                     resetCamera={simulariumController.resetCamera}
@@ -474,6 +509,7 @@ function mapStateToProps(state: State) {
         isBuffering: viewerStateBranch.selectors.getIsBuffering(state),
         isPlaying: viewerStateBranch.selectors.getIsPlaying(state),
         isLooping: viewerStateBranch.selectors.getIsLooping(state),
+        movieTitle: getMovieTitle(state),
     };
 }
 
