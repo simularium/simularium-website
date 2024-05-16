ac6f07d02ef4cebc52a9d0b41d7d4a946665ee1a
meganrm <meganrm@gmail.com>
2024-05-16 15:37:57 -0700

    DRY out code

--- src/components/ConversionErrorModal/index.tsx
+++ src/components/ConversionErrorModal/index.tsx
@@ -2,73 +2,27 @@ import { Button } from "antd";
 import React from "react";
 import classNames from "classnames";
 
-import { AvailableEngines } from "../../state/trajectory/conversion-data-types";
 import CustomModal from "../CustomModal";
 import { Exclamation } from "../Icons";
 
 import styles from "./style.css";
-import { ConversionError } from "../../constants/interfaces";
 
 interface ConversionErrorModalProps {
     closeModal: () => void;
-    conversionError: ConversionError;
-    engineType: AvailableEngines;
+    showForumMessage: boolean;
+    errorMessage?: string;
 }
 
 const ConversionErrorModal: React.FC<ConversionErrorModalProps> = ({
     closeModal,
-    conversionError,
-    engineType,
+    errorMessage,
+    showForumMessage = true,
 }) => {
     const footerButton = (
         <Button className="primary-button" onClick={closeModal}>
             OK
         </Button>
     );
-
-    const getErrorMessage = () => {
-        switch (conversionError) {
-            case ConversionError.SERVER_ERROR:
-                return (
-                    <p>
-                        {`Please try again at a later time. For further assistance, please
-                visit `}
-                        <a
-                            href="https://forum.allencell.org/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            The Allen Cell Discussion Forum.
-                        </a>
-                    </p>
-                );
-            case ConversionError.FILE_TYPE_ERROR:
-                return (
-                    <p>
-                        {`You may want to double check that the file you selected is a
-                    valid `}
-                        {engineType}
-                        {` file and try again. For further
-                    assistance, please visit `}
-                        <a
-                            href="https://forum.allencell.org/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            The Allen Cell Discussion Forum.
-                        </a>
-                    </p>
-                );
-            case ConversionError.FILE_SIZE_ERROR:
-                return (
-                    <p>
-                        {`Your file exceeds the maximum allowed size of 200 MB, please try uploading a smaller file.`}
-                    </p>
-                );
-            default:
-                return null;
-        }
-    };
 
     return (
         <CustomModal
@@ -78,9 +32,23 @@ const ConversionErrorModal: React.FC<ConversionErrorModalProps> = ({
         >
             <p className={classNames(styles.warningText)}>
                 {Exclamation}
-                {` We're sorry, there was a problem importing your file.`}
+                We&apos;re sorry, there was a problem importing your file.
             </p>
-            {getErrorMessage()}
+            <p>
+                {errorMessage}
+                {showForumMessage && (
+                    <>
+                        <>For further assistance, please visit </>
+                        <a
+                            href="https://forum.allencell.org/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            The Allen Cell Discussion Forum.
+                        </a>
+                    </>
+                )}
+            </p>
         </CustomModal>
     );
 };
