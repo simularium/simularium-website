477683b28f57153aa3106811db2648bc791dff44
Joe Heffernan <interim17@gmail.com>
2024-05-23 11:53:57 -0700

    rename data bucket url to download url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,7 +15,7 @@ export const URL_PARAM_BASE_TYPES = "base_types.json";
 export const URL_PARAM_CUSTOM_TYPES = "custom-types";
 // URLs
 export const BASE_API_URL = `/api/${API_VERSION}`;
-export const DATA_BUCKET_URL = `https://${process.env.BACKEND_SERVER_IP}:443/download`;
+export const DOWNLOAD_URL = `https://${process.env.BACKEND_SERVER_IP}:443/download`;
 export const FORUM_URL =
     "https://forum.allencell.org/c/software-code/simularium/";
 // Note that below has "/tags/" unlike FORUM_URL
--- src/state/configure-store.ts
+++ src/state/configure-store.ts
@@ -4,7 +4,7 @@ import { createLogicMiddleware } from "redux-logic";
 
 import {
     BASE_API_URL,
-    DATA_BUCKET_URL,
+    DOWNLOAD_URL,
     UI_TEMPLATE_DOWNLOAD_URL_ROOT,
     UI_TEMPLATE_URL_ROOT,
     URL_PARAM_BASE_TYPES,
@@ -37,7 +37,7 @@ const logics = [
 
 const reduxLogicDependencies = {
     baseApiUrl: BASE_API_URL,
-    plotDataUrl: DATA_BUCKET_URL,
+    plotDataUrl: DOWNLOAD_URL,
     httpClient: axios,
     uiTemplateUrlRoot: UI_TEMPLATE_URL_ROOT,
     uiTemplateDownloadUrlRoot: UI_TEMPLATE_DOWNLOAD_URL_ROOT,
