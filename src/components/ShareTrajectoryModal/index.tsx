003a8f5098ec67cdea226e61992e121a78e80e66
Joe Heffernan <interim17@gmail.com>
2024-07-30 12:46:19 -0700

    update shareTrajectoryModal to include embed snippet and new styling

--- src/components/ShareTrajectoryModal/index.tsx
+++ src/components/ShareTrajectoryModal/index.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Button, Checkbox, Input } from "antd";
-import classNames from "classnames";
+import { Button, Checkbox, Input, Radio, RadioChangeEvent, Space } from "antd";
 
 import { State } from "../../state/types";
 import { TimeUnits } from "../../state/trajectory/types";
@@ -9,10 +8,11 @@ import trajectoryStateBranch from "../../state/trajectory";
 import { getDisplayTimes } from "../../containers/ViewerPanel/selectors";
 import { DisplayTimes } from "../../containers/ViewerPanel/types";
 import CustomModal from "../CustomModal";
-import { Link, Warn } from "../Icons";
-import { URL_PARAM_KEY_TIME } from "../../constants";
+import { DownArrow, Link, Warn } from "../Icons";
+import { URL_PARAM_KEY_FILE_NAME, URL_PARAM_KEY_TIME } from "../../constants";
 import { ButtonClass } from "../../constants/interfaces";
-import { editUrlParams } from "../../util";
+import { copyToClipboard, editUrlParams, getUrlParamValue } from "../../util";
+import VerticalFlexbox from "../../styles/utils";
 
 import styles from "./style.css";
 
@@ -29,25 +29,19 @@ const ShareTrajectoryModal = ({
     timeUnits,
     displayTimes,
 }: ShareTrajectoryModalProps): JSX.Element => {
-    const currentTime = roundToTimestepPrecision(
-        displayTimes.roundedTime,
-        displayTimes.roundedTimeStep
-    );
+    /** URL Link */
+    const roundToTimestepPrecision = (
+        input: number,
+        timestep: number
+    ): number => {
+        const precision = (timestep.toString().split(".")[1] || "").length;
+        const multiplier = Math.pow(10, precision);
+        return Math.round(input * multiplier) / multiplier;
+    };
 
-    const [allowTimeInput, setAllowTimeInput] = React.useState(true);
-    const [url, setUrl] = React.useState(
-        editUrlParams(
-            window.location.href,
-            currentTime.toString(),
-            URL_PARAM_KEY_TIME
-        )
-    );
-    const [lastEnteredNumber, setLastEnteredNumber] =
-        React.useState(currentTime);
-
-    const handleAllowUserInput = (): void => {
+    const handleAllowTimeInput = (): void => {
         const timeValue = allowTimeInput ? lastEnteredNumber : currentTime;
-        setUrl(
+        setLinkUrl(
             editUrlParams(
                 window.location.href,
                 timeValue.toString(),
@@ -57,13 +51,7 @@ const ShareTrajectoryModal = ({
         setAllowTimeInput((allowTimeInput) => !allowTimeInput);
     };
 
-    function roundToTimestepPrecision(input: number, timestep: number): number {
-        const precision = (timestep.toString().split(".")[1] || "").length;
-        const multiplier = Math.pow(10, precision);
-        return Math.round(input * multiplier) / multiplier;
-    }
-
-    const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleTimeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputAsNumber = parseFloat(e.target.value);
         let timeValue = null;
         if (Number.isNaN(inputAsNumber)) {
@@ -84,7 +72,7 @@ const ShareTrajectoryModal = ({
             );
         }
         setLastEnteredNumber(timeValue);
-        setUrl(
+        setLinkUrl(
             editUrlParams(
                 window.location.href,
                 timeValue.toString(),
@@ -93,18 +81,217 @@ const ShareTrajectoryModal = ({
         );
     };
 
-    const copyToClipboard = async (): Promise<void> => {
-        try {
-            await navigator.clipboard.writeText(url);
-        } catch (err) {
-            console.error("Failed to copy text: ", err);
-        }
+    const [timeSettingRadioValue, setTimeSettingRadioValue] = React.useState(1);
+    const [allowTimeInput, setAllowTimeInput] = React.useState(true);
+    const currentTime = roundToTimestepPrecision(
+        displayTimes.roundedTime,
+        displayTimes.roundedTimeStep
+    );
+    const [linkUrl, setLinkUrl] = React.useState(
+        editUrlParams(
+            window.location.href,
+            currentTime.toString(),
+            URL_PARAM_KEY_TIME
+        )
+    );
+    const [lastEnteredNumber, setLastEnteredNumber] =
+        React.useState(currentTime);
+    const onRadioChange = (e: RadioChangeEvent) => {
+        setTimeSettingRadioValue(e.target.value);
+        handleAllowTimeInput();
     };
+
+    /** Embedded Snippet */
+    interface EmbedSettings {
+        width: number;
+        height: number;
+        userProvidedProportions: boolean;
+    }
+
+    const defaultEmbedSettings: EmbedSettings = {
+        width: 834,
+        height: 480,
+        userProvidedProportions: false,
+    };
+
+    const trajectory = getUrlParamValue(
+        window.location.href,
+        URL_PARAM_KEY_FILE_NAME
+    );
+    const [embedSettings, setEmbedSettings] =
+        React.useState(defaultEmbedSettings);
+    const [showEmbedSettingsPanel, setShowEmbedSettingsPanel] =
+        React.useState(false);
+
+    const generateEmbedSnippet = (): string => {
+        const { width, height } = embedSettings;
+
+        const snippetHeight =
+            embedSettings.userProvidedProportions && embedSettings.height
+                ? height
+                : defaultEmbedSettings.height;
+        const snippetWidth =
+            embedSettings.userProvidedProportions && embedSettings.width
+                ? width
+                : defaultEmbedSettings.width;
+        const url = `https://simularium.allencell.org/embed?trajFileName=${trajectory}&t=0`;
+        return `<iframe height="${snippetHeight}" width="${snippetWidth}"src="${url}" title="Simularium" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    };
+
+    const [embedSnippet, setEmbedSnippet] = React.useState(
+        generateEmbedSnippet()
+    );
+
+    useEffect(() => {
+        setEmbedSnippet(generateEmbedSnippet());
+    }, [embedSettings]);
+
+    const handleEmbeddedSizeInputs = (
+        setting: "height" | "width",
+        input: string
+    ) => {
+        const numericInput = input ? parseInt(input) : 0;
+        const roundedLimitedInput = numericInput >= 3840 ? 3840 : numericInput;
+        setEmbedSettings({ ...embedSettings, [setting]: roundedLimitedInput });
+    };
+
+    /** JSX Elements for sections: URL Link and Embed Snippet, and Error/Shareable options */
+
+    const ShareableLinkPanel: JSX.Element = (
+        <>
+            <VerticalFlexbox gap={2}>
+                <Radio.Group
+                    className={styles.customRadioGroup}
+                    value={timeSettingRadioValue}
+                    onChange={onRadioChange}
+                >
+                    <Space direction="vertical">
+                        <Radio value={1}>From beginning</Radio>
+                        <Radio value={2}>
+                            Start at{" "}
+                            <Input
+                                className={styles.timeInput}
+                                disabled={allowTimeInput}
+                                defaultValue={currentTime}
+                                onChange={handleTimeInput}
+                            />
+                            <div>
+                                /{displayTimes.roundedLastFrameTime}
+                                {timeUnits ? timeUnits.name : null}
+                            </div>{" "}
+                        </Radio>
+                    </Space>
+                </Radio.Group>
+            </VerticalFlexbox>
+            <VerticalFlexbox gap={10}>
+                <h4> Copy link {Link} </h4>
+                <div className={styles.urlInputContainer}>
+                    <Input
+                        className={styles.urlInput}
+                        value={linkUrl}
+                        disabled
+                    />
+                    <Button
+                        className={"primary-button"}
+                        onClick={() => copyToClipboard(linkUrl)}
+                    >
+                        Copy
+                    </Button>
+                </div>
+            </VerticalFlexbox>
+        </>
+    );
+
+    const EmbedSnippetPanel: JSX.Element = (
+        <VerticalFlexbox gap={8}>
+            <div className={styles.embedHeader}>
+                <h4>Embed &lt;/&gt;</h4>
+                <button
+                    className={styles.settingsButton}
+                    onClick={() => {
+                        setShowEmbedSettingsPanel(!showEmbedSettingsPanel);
+                    }}
+                >
+                    Show settings {DownArrow}
+                </button>
+            </div>
+            {showEmbedSettingsPanel && (
+                <div className={styles.embedSettings}>
+                    <VerticalFlexbox gap={4}>
+                        <p className={styles.accentText}> Size </p>
+                        <VerticalFlexbox gap={24}>
+                            <div className={styles.proportionSettings}>
+                                {" "}
+                                <Input
+                                    key="embed-height"
+                                    type="text"
+                                    inputMode="numeric"
+                                    disabled={
+                                        !embedSettings.userProvidedProportions
+                                    }
+                                    defaultValue={embedSettings.height}
+                                    className={styles.numberInputs}
+                                    onChange={(e) => {
+                                        handleEmbeddedSizeInputs(
+                                            "height",
+                                            e.target.value
+                                        );
+                                    }}
+                                />
+                                <p> x </p>
+                                <Input
+                                    key="embed-width"
+                                    type="text"
+                                    disabled={
+                                        !embedSettings.userProvidedProportions
+                                    }
+                                    value={embedSettings.width}
+                                    className={styles.numberInputs}
+                                    onChange={(e) => {
+                                        handleEmbeddedSizeInputs(
+                                            "width",
+                                            e.target.value
+                                        );
+                                    }}
+                                />
+                                <p> pixels </p>
+                                <div className={styles.constrainProportions}>
+                                    <Checkbox
+                                        onChange={() => {
+                                            setEmbedSettings({
+                                                ...embedSettings,
+                                                ["userProvidedProportions"]:
+                                                    !embedSettings.userProvidedProportions,
+                                            });
+                                        }}
+                                    ></Checkbox>
+                                    <p>Constrain proportions</p>
+                                </div>
+                            </div>
+                        </VerticalFlexbox>
+                    </VerticalFlexbox>
+                </div>
+            )}
+            <VerticalFlexbox gap={8} alignItems="end">
+                <Input.TextArea
+                    className={styles.embedInput}
+                    value={embedSnippet}
+                    disabled
+                />
+                <Button
+                    className={"primary-button"}
+                    onClick={() => copyToClipboard(embedSnippet)}
+                >
+                    Copy
+                </Button>
+            </VerticalFlexbox>
+        </VerticalFlexbox>
+    );
 
     const modalOptions = {
         errorMessage: {
             content: (
-                <div className={styles.errorContainer}>
+                <div>
                     <h4>{Warn} The current file is stored on your device.</h4>
                     <div>
                         <h5>
@@ -131,42 +318,14 @@ const ShareTrajectoryModal = ({
         },
         isShareable: {
             content: (
-                <div className={styles.shareContainer}>
-                    <div className={styles.urlInputContainer}>
-                        <Input
-                            className={styles.urlInput}
-                            value={url}
-                            disabled
-                        />
-                        <Button
-                            className={classNames(
-                                "primary-button",
-                                styles.copyButton
-                            )}
-                            onClick={copyToClipboard}
-                        >
-                            Copy {Link}
-                        </Button>
-                    </div>
-                    <div className={styles.timeInputContainer}>
-                        <Checkbox onChange={handleAllowUserInput}></Checkbox>
-                        <p className={styles.timeInputText}>Start at</p>
-                        <Input
-                            className={styles.timeInput}
-                            disabled={allowTimeInput}
-                            defaultValue={currentTime}
-                            onChange={handleUserInput}
-                        />
-                        <div>
-                            /{displayTimes.roundedLastFrameTime}
-                            {timeUnits ? timeUnits.name : null}
-                        </div>
-                    </div>
-                </div>
+                <VerticalFlexbox gap={30} className={styles.shareContainer}>
+                    {ShareableLinkPanel}
+                    {EmbedSnippetPanel}
+                </VerticalFlexbox>
             ),
             footer: (
                 <Button className={ButtonClass.Secondary} onClick={closeModal}>
-                    Close
+                    Done
                 </Button>
             ),
         },
@@ -175,9 +334,7 @@ const ShareTrajectoryModal = ({
     return (
         <CustomModal
             closeHandler={closeModal}
-            className={styles.uploadModal}
             titleText="Share trajectory"
-            divider={true}
             width={trajectoryIsShareable ? 550 : 611}
             footerButtons={
                 trajectoryIsShareable
