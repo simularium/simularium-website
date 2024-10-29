aeaeeea1408f4689da867a8d9f4bdaeafb446e49
Joe Heffernan <interim17@gmail.com>
2024-10-29 12:53:38 -0700

    add colors constants in separate file

---
+++ src/styles/theme/themeColors.ts
@@ -0,0 +1,36 @@
+/**
+ * Constants defined to replace ant-vars.less
+ * For now removing defining the basic colors
+ * and some global and <body> specific assignments.
+ *
+ *
+ * All subject to review after implementing theme in ant5,
+ * this is more or less a place holder, to show how the new
+ * constants will be organized.
+ */
+
+export const DARK = "#141219";
+export const DARK_TWO = "#1e1b25";
+export const DARK_THREE = "#25222e";
+export const DARK_FOUR = "#2f2a3c";
+export const WHITE_SIX = "#e7e7e7";
+export const BABY_PURPLE = "#b59ff6";
+export const LIGHT_PURPLE = "#e7e4f2";
+export const DARK_PURPLE = "#3b3649";
+export const ALLEN_PURPLE = "#8d87aa";
+export const GRAYISH_BROWN = "#4a4a4a";
+export const HIGHLIGHT_GREEN = "#b2d030";
+export const BLUE = "#0094FF";
+export const MEDIUM_DARK_GRAY = "#8b8b8b";
+export const PALE_GRAY = "#ddd9ec";
+export const WARM_GRAY = "#979797";
+export const HEATHER = "#bab5c9";
+
+export const PRIMARY_COLOR = BABY_PURPLE;
+export const HIGHLIGHT_COLOR = BABY_PURPLE;
+export const BACKGROUND_COLOR = WHITE_SIX;
+
+export const BODY_BACKGROUND = "#f7f7f7";
+export const COMPONENT_BACKGROUND = "#f7f7f7";
+export const TEXT_COLOR = GRAYISH_BROWN;
+export const BACKGROUND_COLOR_LIGHT = "#302c3b"; // From lighten(@dark-three, 10%)
