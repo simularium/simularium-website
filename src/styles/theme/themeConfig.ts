0baeeed49c21d2a26c20f54b08bdd2d1038ac55d
Joe Heffernan <interim17@gmail.com>
2024-10-29 12:39:35 -0700

    wrap app in config provider to use antd design tokens

---
+++ src/styles/theme/themeConfig.ts
@@ -0,0 +1,10 @@
+import { ThemeConfig } from "antd";
+
+export const simulariumTheme: ThemeConfig = {
+    token: {
+        fontFamily:
+            "'Overpass', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif",
+    },
+};
+
+export default simulariumTheme;
