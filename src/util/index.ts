2999d03771eaaabbc5bfa63977c5244f9986a788
Joe Heffernan <interim17@gmail.com>
2024-11-04 12:19:20 -0800

    remove unused bindAll function

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,22 +1,5 @@
-import { forOwn, isFunction } from "lodash";
-import React from "react";
 import { IconGlyphs } from "../constants/interfaces";
 import { UIDisplayData } from "@aics/simularium-viewer";
-
-type AnyFunction = () => any;
-
-export function bindAll<T extends React.Component>(
-    obj: T,
-    methods: AnyFunction[]
-) {
-    const setOfMethods = new Set(methods);
-
-    forOwn(obj.constructor.prototype, (value, key) => {
-        if (setOfMethods.has(value) && isFunction(value)) {
-            Object.assign(obj, { [key]: value.bind(obj) });
-        }
-    });
-}
 
 export function convertToSentenceCase(string: string): string {
     if (!string) {
